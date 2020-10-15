2dd606c798a17607609370787f8eb52f683d76c3
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-15 11:19:38 +0100

    feat: add urls lang

---
+++ resources/lang/en/urls.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'slack' => 'https://slack.ark.io/',
+    'discord' => 'https://discord.ark.io/',
+    'twitter' => 'https://twitter.ark.io/',
+    'linkedin' => 'https://www.linkedin.com/company/ark-ecosystem',
+    'facebook' => 'https://facebook.ark.io/',
+    'reddit' => 'https://reddit.ark.io/',
+    'youtube' => 'https://youtube.ark.io/',
+    'github' => 'https://github.com/ArkEcosystem',
+];
--- resources/views/footer-social.blade.php
+++ resources/views/footer-social.blade.php
@@ -1,10 +1,10 @@
 <div class="flex space-x-5">
-    <x-ark-social-link :url="trans('urls.slack')" icon="brands.slack" />
-    <x-ark-social-link :url="trans('urls.discord')" icon="brands.discord" />
-    <x-ark-social-link :url="trans('urls.twitter')" icon="brands.twitter" />
-    <x-ark-social-link :url="trans('urls.linkedin')" icon="brands.linkedin" />
-    <x-ark-social-link :url="trans('urls.facebook')" icon="brands.facebook" />
-    <x-ark-social-link :url="trans('urls.reddit')" icon="brands.reddit" />
-    <x-ark-social-link :url="trans('urls.youtube')" icon="brands.youtube" />
-    <x-ark-social-link :url="trans('urls.github')" icon="brands.github" />
+    <x-ark-social-link :url="trans('ui::urls.slack')" icon="brands.slack" />
+    <x-ark-social-link :url="trans('ui::urls.discord')" icon="brands.discord" />
+    <x-ark-social-link :url="trans('ui::urls.twitter')" icon="brands.twitter" />
+    <x-ark-social-link :url="trans('ui::urls.linkedin')" icon="brands.linkedin" />
+    <x-ark-social-link :url="trans('ui::urls.facebook')" icon="brands.facebook" />
+    <x-ark-social-link :url="trans('ui::urls.reddit')" icon="brands.reddit" />
+    <x-ark-social-link :url="trans('ui::urls.youtube')" icon="brands.youtube" />
+    <x-ark-social-link :url="trans('ui::urls.github')" icon="brands.github" />
 </div>
