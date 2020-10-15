fde0cd21ca58e34fdf7e474d1e4b78917b1a34c0
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2020-10-15 17:39:22 +0200

    fix: link collection missing styles and props (#6)

--- resources/views/link-collection.blade.php
+++ resources/views/link-collection.blade.php
@@ -1,7 +1,7 @@
 <div class="grid grid-flow-row grid-cols-1 link-collection gap-x-3 sm:grid-cols-2 lg:grid-cols-3">
     @foreach ($links as $link)
         <div class="py-1">
-            <a href="{{ $link['path'] }}" class="flex items-center justify-between w-full px-2 py-3 rounded transition-default text-theme-primary-600 hover:bg-theme-primary-100 hover:text-theme-primary-700">
+            <a href="{{ $link[ $urlProperty ?? 'path'] }}" class="flex items-center justify-between w-full px-2 py-3 rounded transition-default text-theme-primary-600 hover:bg-theme-primary-100 hover:text-theme-primary-700" @if ($isExternal ?? false) target="_blank" rel="noopener nofollow noreferrer" @endif>
                 <span>{{ $link['name'] }}</span>
                 @svg('arrow-right', 'h-6 w-6')
             </a>
