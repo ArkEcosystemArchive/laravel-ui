ddb700d68944f32e020d335a3de3b73e5c4a8040
alexbarnsley <alexbarnsley@users.noreply.github.com>
2020-10-20 09:48:30 +0000

    style: resolve style guide violations

--- resources/assets/js/page-scroll.js
+++ resources/assets/js/page-scroll.js
@@ -1,8 +1,8 @@
-window.scrollToQuery = selector => {
-    const navbar = document.querySelector('#navbar');
+window.scrollToQuery = (selector) => {
+    const navbar = document.querySelector("#navbar");
 
     window.scrollTo({
         top: document.querySelector(selector).offsetTop - navbar.clientHeight,
-        behavior: 'smooth',
+        behavior: "smooth",
     });
 };
