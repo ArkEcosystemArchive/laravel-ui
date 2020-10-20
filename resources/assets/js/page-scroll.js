e59961d24413523f4410f28dc1a0f24e894f39a4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-20 10:36:23 +0100

    chore: page scroll js

---
+++ resources/assets/js/page-scroll.js
@@ -0,0 +1,8 @@
+window.scrollToQuery = selector => {
+    const navbar = document.querySelector('#navbar');
+
+    window.scrollTo({
+        top: document.querySelector(selector).offsetTop - navbar.clientHeight,
+        behavior: 'smooth',
+    });
+};
