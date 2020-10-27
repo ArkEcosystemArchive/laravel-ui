985bd9f9c5b9a034fafc2c089f60eb8eaff468a6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-27 13:24:49 +0000

    refactor: mobile footer

--- resources/views/footer-copyright.blade.php
+++ resources/views/footer-copyright.blade.php
@@ -1,3 +1,7 @@
-<div class="py-6 font-semibold text-theme-secondary-700">
-    {{ date('Y') }} &copy; ARK.io | All rights reserved | @svg('ark-logo-red-square', 'inline-block h-6 -mt-1') An <a href="https://ark.io/" class="underline hover:no-underline">ARK.io</a> Product
+<div class="py-6 font-semibold text-theme-secondary-700 flex flex-col space-y-2 md:block md:space-y-0">
+    <span class="whitespace-no-wrap">{{ date('Y') }} &copy; ARK.io | All rights reserved</span>
+    <span class="hidden md:inline"> | </span>
+    <span class="whitespace-no-wrap">
+        @svg('ark-logo-red-square', 'inline-block h-6 -mt-1') An <a href="https://ark.io/" class="underline hover:no-underline">ARK.io</a> Product
+    </span>
 </div>
