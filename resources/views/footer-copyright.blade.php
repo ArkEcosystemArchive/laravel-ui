c38b405f434a4d52c1dbd831071f15e1822767de
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-28 19:28:30 +0000

    refactor: footer wrapping larger device

--- resources/views/footer-copyright.blade.php
+++ resources/views/footer-copyright.blade.php
@@ -1,6 +1,6 @@
-<div class="flex flex-col py-6 space-y-2 font-semibold text-theme-secondary-700 md:block md:space-y-0">
+<div class="flex flex-col py-6 space-y-2 font-semibold text-theme-secondary-700 sm:block sm:space-y-0">
     <span class="whitespace-no-wrap">{{ date('Y') }} &copy; ARK.io | All rights reserved</span>
-    <span class="hidden md:inline"> | </span>
+    <span class="hidden mr-1 sm:inline"> | </span>
     <span class="whitespace-no-wrap">
         @svg('ark-logo-red-square', 'inline-block h-6 -mt-1') An <a href="https://ark.io/" class="underline hover:no-underline">ARK.io</a> Product
     </span>
