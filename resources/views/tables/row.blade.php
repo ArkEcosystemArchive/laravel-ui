d9b113c93407f3614995eca3baadb1188ea622a0
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-11-12 10:46:59 -0600

    Attributes

--- resources/views/tables/row.blade.php
+++ resources/views/tables/row.blade.php
@@ -2,6 +2,6 @@
     'danger' => false,
     'warning' => false,
 ])
-<tr @if($danger) data-danger @elseif($warning) data-warning @endif>
+<tr {{ $attributes }} @if($danger) data-danger @elseif($warning) data-warning @endif>
     {{ $slot }}
 </tr>
