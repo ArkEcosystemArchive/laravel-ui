152772aa46dc492a62fc61885256aac8271a9219
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-12 14:06:27 +0000

    chore: menu & search icon updates (#59)

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -77,8 +77,13 @@
                         {{-- Mobile Hamburger icon --}}
                         <div class="flex items-center pr-6 border-r border-theme-secondary-300 md:hidden">
                             <button @click="open = !open" class="inline-flex items-center justify-center p-2 transition duration-150 ease-in-out rounded-md text-theme-secondary-900">
-                                <span :class="{'hidden': open, 'inline-flex': !open }">@svg('menu-open', 'h-4 w-4')</span>
-                                <span :class="{'hidden': !open, 'inline-flex': open }" x-cloak>@svg('menu-close', 'h-4 w-4')</span>
+                                <span :class="{'hidden': open, 'inline-flex': !open }">
+                                    <x-ark-icon name="menu" size="sm" />
+                                </span>
+
+                                <span :class="{'hidden': !open, 'inline-flex': open }" x-cloak>
+                                    <x-ark-icon name="menu-show" size="sm" />
+                                </span>
                             </button>
                         </div>
 
