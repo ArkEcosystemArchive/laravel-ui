8e88dad7dc2d82d5c82afcd3bf97ff3b31b3cb90
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-11-12 10:34:03 -0600

    Add some comments

--- resources/views/tables/cell.blade.php
+++ resources/views/tables/cell.blade.php
@@ -1,7 +1,17 @@
 @props([
     'responsive' => false,
     'breakpoint' => 'lg',
+    // Set to true for the first column.
+    // Only needed if the first column dissapears in resposive versions
+    'first' => false,
+    // In wich screen sizes this column will be the first one
+    // Expects breakpoints as string `xl`, `lg`, etc
+    'firstOn' => null,
+    // Set to true for the last column.
+    // Only needed if the last column dissapears in resposive versions
     'last' => false,
+    // In wich screen sizes this column will be the last one
+    // Expects breakpoints as string `xl`, `lg`, etc
     'lastOn' => null,
     'class' => '',
 ])
@@ -11,6 +21,7 @@
         't-cell'
         . ($responsive ? ' ' . $breakpoint . ':table-cell hidden' : '')
         . ($last || $lastOn ? (' last-cell' . (is_string($lastOn) ? ' last-cell-' . $lastOn : '')) : '')
+        . ($first || $firstOn ? (' first-cell' . (is_string($firstOn) ? ' first-cell-' . $firstOn : '')) : '')
         . ' ' . $class
 ]) }}>
     <div class="relative flex items-center w-full h-full px-3 py-4">
