6249a3928fb17f052aee6953687d6fddf9e5d3b8
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-11-12 10:51:21 -0600

    Simplify last & first definition

--- resources/views/tables/cell.blade.php
+++ resources/views/tables/cell.blade.php
@@ -1,17 +1,11 @@
 @props([
     'responsive' => false,
     'breakpoint' => 'lg',
-    // Set to true for the first column.
-    // Only needed if the first column dissapears in resposive versions
-    'first' => false,
-    // In wich screen sizes this column will be the first one
-    // Expects breakpoints as string `xl`, `lg`, etc
+    // In wich screen sizes this column will be the first one  (`xl`, `lg`, etc)
+    // (Only neccesary if the first column changes on responsive versions)
     'firstOn' => null,
-    // Set to true for the last column.
-    // Only needed if the last column dissapears in resposive versions
-    'last' => false,
-    // In wich screen sizes this column will be the last one
-    // Expects breakpoints as string `xl`, `lg`, etc
+    // In wich screen sizes this column will be the last one (`xl`, `lg`, etc)
+    // (Only neccesary if the last column changes on responsive versions)
     'lastOn' => null,
     'class' => '',
 ])
@@ -20,8 +14,8 @@
     'class' =>
         't-cell'
         . ($responsive ? ' ' . $breakpoint . ':table-cell hidden' : '')
-        . ($last || $lastOn ? (' last-cell' . (is_string($lastOn) ? ' last-cell-' . $lastOn : '')) : '')
-        . ($first || $firstOn ? (' first-cell' . (is_string($firstOn) ? ' first-cell-' . $firstOn : '')) : '')
+        . ($lastOn ? ' last-cell last-cell-' . $lastOn : '')
+        . ($firstOn ? ' first-cell first-cell-' . $firstOn : '')
         . ' ' . $class
 ]) }}>
     <div class="relative flex items-center w-full h-full px-3 py-4">
