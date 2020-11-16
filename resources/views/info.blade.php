222071717701e67251ce381a2e34ebb9ddeff3ae
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-16 18:58:51 +0000

    feat: add info tooltip component (#71)

---
+++ resources/views/info.blade.php
@@ -0,0 +1,6 @@
+<div
+    data-tippy-content="{{ $tooltip }}"
+    class="inline-block px-1 py-1 rounded-full bg-theme-primary-100 text-theme-primary-600 dark:bg-theme-secondary-800 dark:text-theme-secondary-600"
+>
+    <x-ark-icon name="question-mark" size="2xs" />
+</div>
