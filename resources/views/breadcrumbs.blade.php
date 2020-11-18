b2f1c323054eae430a8198e959d2c3c548f82a8f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-18 09:48:07 +0000

    refactor: remove breadcrumb class options (#78)

--- resources/views/breadcrumbs.blade.php
+++ resources/views/breadcrumbs.blade.php
@@ -1,5 +1,5 @@
-<div class="h-16 w-full flex justify-center {{ $wrapperClass ?? 'bg-theme-secondary-100 dark:bg-black' }}">
-    <div class="w-full h-full flex items-center justify-start text-sm text-theme-secondary-500 leading-relaxed {{ $innerClass ?? 'px-4 sm:px-8' }}">
+<div class="flex justify-center w-full h-16 bg-theme-secondary-100 dark:bg-black">
+    <div class="flex items-center justify-start w-full h-full px-8 mx-auto text-sm leading-relaxed text-theme-secondary-500 max-w-7xl">
         @foreach ($crumbs as $crumb)
             @if(isset($crumb['route']))
                 <span>
