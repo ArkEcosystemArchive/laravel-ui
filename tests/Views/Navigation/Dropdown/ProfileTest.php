0f32e37a0e662c7db9ca36b858b8ed05ec5356b1
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-18 14:17:26 +0200

    refactor: make the navbar more modular (#79)

---
+++ tests/Views/Navigation/Dropdown/ProfileTest.php
@@ -0,0 +1,31 @@
+<?php
+
+it('should render the component', function (): void {
+    $this
+        ->assertView('ark::navbar.dropdown.profile', [
+            'profilePhoto'     => 'https://imgur.com/abc123',
+            'profileMenu'      => [],
+        ])
+        ->contains('src="https://imgur.com/abc123"');
+});
+
+it('should render the [profileMenuClass] attribute', function (): void {
+    $this
+        ->assertView('ark::navbar.dropdown.profile', [
+            'profilePhoto'     => 'https://imgur.com/abc123',
+            'profileMenu'      => [],
+            'profileMenuClass' => 'unicorn',
+        ])
+        ->contains('src="https://imgur.com/abc123"')
+        ->contains('unicorn');
+});
+
+it('should render the [identifier] attribute instead of the [profilePhoto] attribute', function (): void {
+    $this
+        ->assertView('ark::navbar.dropdown.profile', [
+            'profilePhoto' => 'https://imgur.com/abc123',
+            'profileMenu'  => [],
+            'identifier'   => 'unicorn',
+        ])
+        ->contains('x-html="avatarImage"');
+});
