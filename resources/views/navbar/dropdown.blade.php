cd2c225d0c28098c2675ffc1ff5a5ac533da2e0b
Brian Faust <hello@basecode.sh>
2020-11-19 06:01:31 +0200

    wip

--- resources/views/navbar/dropdown.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-4 sm:pr-0">
-    <x-ark-navbar-hamburger />
-
-    @isset($dropdownContent)
-        {{ $dropdownContent }}
-    @else
-        @include('ark::navbar.dropdown.content')
-    @endisset
-</div>
