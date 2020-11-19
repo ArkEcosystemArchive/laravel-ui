b7722acc687503ae68c4884304da95601f7125a1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-19 20:31:21 +0000

    fix: shallow circle icon remove icon property

--- resources/views/status-circle-shallow.blade.php
+++ resources/views/status-circle-shallow.blade.php
@@ -1,6 +1,6 @@
 @if ($type === 'success')
     <div class="flex items-center justify-center flex-shrink-0 w-6 h-6 border-2 rounded-full border-theme-success-600">
-        @svg($icon, 'text-theme-success-600 h-3 w-3')
+        @svg('checkmark', 'text-theme-success-600 h-2 w-2')
     </div>
 @elseif ($type === 'failed')
     <div class="flex items-center justify-center flex-shrink-0 w-6 h-6 border-2 rounded-full border-theme-danger-400">
