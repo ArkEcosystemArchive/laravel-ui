32f6dd054331772066091c6c3017e95fe84bd156
Highjhacker <jolanbeer@gmail.com>
2020-11-23 17:46:40 +0100

    refactor: navbar notification icon tweaks

--- resources/views/navbar/notifications.blade.php
+++ resources/views/navbar/notifications.blade.php
@@ -1,7 +1,7 @@
 <div @click="livewire.emit('markNotificationsAsSeen')">
-    <x-ark-dropdown wrapper-class="ml-3 sm:relative" dropdown-classes="mt-6 md:mt-8 {{ $dropdownClasses ?? '' }}">
+    <x-ark-dropdown wrapper-class="mx-3 sm:relative" dropdown-classes="mt-6 md:mt-8 {{ $dropdownClasses ?? '' }}">
         <x-slot name="button">
-            @svg('notification', 'h-6 w-6 transition-default')
+            @svg('notification', 'h-6 w-6 transition-default text-theme-secondary-400 hover:text-theme-primary-600')
 
             @isset($notificationsIndicator)
                 {{ $notificationsIndicator }}
