93637b8c20d63cfc49556d451c8bbc631c51fc38
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-23 11:35:55 +0000

    feat: dropdown reposition on bottom overflow (#97)

---
+++ resources/assets/js/reposition-dropdown.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const dropdownButtons = document.querySelectorAll(".dropdown-button");
+    for (const button of dropdownButtons) {
+        if (
+            !button ||
+            (button.getBoundingClientRect().height === 0 &&
+                button.getBoundingClientRect().width === 0)
+        ) {
+            continue;
+        }
+
+        const bounds = button.getBoundingClientRect();
+        const dropdown = button.parentElement.parentElement.querySelector(
+            ".dropdown"
+        );
+        if (!dropdown || !dropdown.dataset.height) {
+            continue;
+        }
+
+        const dropdownBottom =
+            bounds.top +
+            bounds.height +
+            document.documentElement.scrollTop +
+            parseInt(dropdown.dataset.height);
+        if (dropdownBottom > document.body.clientHeight) {
+            dropdown.style =
+                "display: none; margin-top: -" +
+                (dropdownBottom - document.body.clientHeight + 20) +
+                "px";
+        }
+    }
+});
