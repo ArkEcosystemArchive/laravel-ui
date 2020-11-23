c714fa8de7c2d915887f3b847677b37521e0c4b8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-23 11:30:03 +0000

    refactor: additions for dropdowns height/position

--- resources/views/dropdown.blade.php
+++ resources/views/dropdown.blade.php
@@ -12,7 +12,7 @@
         <button
             @click="{{ $dropdownProperty }} = !{{ $dropdownProperty }}"
             :class="{ 'text-theme-primary-500' : {{ $dropdownProperty }} }"
-            class="flex text-theme-secondary-400 items-center hover:text-theme-primary-500 focus:outline-none p-3 {{ $buttonClass ?? '' ? $buttonClass : '' }}"
+            class="flex text-theme-secondary-400 items-center hover:text-theme-primary-500 focus:outline-none p-3 dropdown-button {{ $buttonClass ?? '' ? $buttonClass : '' }}"
         >
             @if($button ?? false)
                 {{ $button }}
@@ -21,6 +21,7 @@
             @endif
         </button>
     </div>
+
     <div
         x-show="{{ $dropdownProperty }}"
         x-transition:enter="transition ease-out duration-100"
@@ -29,7 +30,8 @@
         x-transition:leave="transition ease-in duration-75"
         x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95"
-        class="origin-top-right absolute right-0 mt-2 rounded-md shadow-lg z-10 {{ $dropdownClasses ?? 'w-40' }}"
+        class="origin-top-right absolute right-0 mt-2 rounded-md shadow-lg z-10 dropdown {{ $dropdownClasses ?? 'w-40' }}"
+        @if ($height ?? false) data-height="{{ $height }}" @endif
     >
         <div class="bg-white rounded-md shadow-xs dark:bg-theme-secondary-800 dark:text-theme-secondary-200" x-cloak>
             <div class="py-1" @if($closeOnClick ?? true) @click="{{ $dropdownProperty }} = !{{ $dropdownProperty }}" @endif>
