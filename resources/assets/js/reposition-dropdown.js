566cbcaa4cc8eba930bff9c7941f110b9d001258
alexbarnsley <alexbarnsley@users.noreply.github.com>
2020-11-23 11:31:02 +0000

    style: resolve style guide violations

--- resources/assets/js/reposition-dropdown.js
+++ resources/assets/js/reposition-dropdown.js
@@ -1,19 +1,32 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const dropdownButtons = document.querySelectorAll('.dropdown-button');
+window.addEventListener("DOMContentLoaded", () => {
+    const dropdownButtons = document.querySelectorAll(".dropdown-button");
     for (const button of dropdownButtons) {
-        if (! button || (button.getBoundingClientRect().height === 0 && button.getBoundingClientRect().width === 0)) {
+        if (
+            !button ||
+            (button.getBoundingClientRect().height === 0 &&
+                button.getBoundingClientRect().width === 0)
+        ) {
             continue;
         }
 
-        const bounds   = button.getBoundingClientRect();
-        const dropdown = button.parentElement.parentElement.querySelector('.dropdown');
-        if (!dropdown || ! dropdown.dataset.height) {
+        const bounds = button.getBoundingClientRect();
+        const dropdown = button.parentElement.parentElement.querySelector(
+            ".dropdown"
+        );
+        if (!dropdown || !dropdown.dataset.height) {
             continue;
         }
 
-        const dropdownBottom = bounds.top + bounds.height + document.documentElement.scrollTop + parseInt(dropdown.dataset.height);
+        const dropdownBottom =
+            bounds.top +
+            bounds.height +
+            document.documentElement.scrollTop +
+            parseInt(dropdown.dataset.height);
         if (dropdownBottom > document.body.clientHeight) {
-            dropdown.style = 'display: none; margin-top: -' + ((dropdownBottom - document.body.clientHeight) + 20) + 'px';
+            dropdown.style =
+                "display: none; margin-top: -" +
+                (dropdownBottom - document.body.clientHeight + 20) +
+                "px";
         }
     }
 });
