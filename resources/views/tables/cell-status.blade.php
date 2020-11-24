caa5e24e524c382d8b7a39f5db8220b2586c4aa5
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-24 09:51:21 +0000

    feat: table cell status component

---
+++ resources/views/tables/cell-status.blade.php
@@ -0,0 +1,56 @@
+@props([
+    'responsive' => false,
+    'breakpoint' => 'lg',
+    // In wich screen sizes this column will be the first one  (`xl`, `lg`, etc)
+    // (Only neccesary if the first column changes on responsive versions)
+    'firstOn' => null,
+    // In wich screen sizes this column will be the last one (`xl`, `lg`, etc)
+    // (Only neccesary if the last column changes on responsive versions)
+    'lastOn' => null,
+    'class' => '',
+    'colspan' => null,
+    'type' => null,
+])
+
+@php
+    $colours = [
+        'success' => 'bg-theme-success-50 text-theme-success-400',
+        'failed'  => 'bg-theme-danger-50 text-theme-danger-400',
+        'error'   => 'bg-theme-danger-50 text-theme-danger-400',
+        'debug'   => 'bg-theme-hint-50 text-theme-hint-500',
+        'running' => 'bg-theme-warning-50 text-theme-warning-400',
+        'updated' => 'bg-theme-warning-50 text-theme-warning-400',
+        'active'  => 'bg-theme-primary-50 text-theme-primary-400',
+        'locked'  => 'bg-theme-secondary-50 text-theme-secondary-400',
+        'none'    => '',
+    ][$type ?? 'none'];
+@endphp
+
+<td {{ $attributes->merge([
+    'class' =>
+        'hoverable-cell'
+        . ($responsive && !$breakpoint ? ' hidden lg:table-cell' : '')
+        . ($responsive && $breakpoint === 'xl' ? ' hidden xl:table-cell' : '')
+        . ($responsive && $breakpoint === 'lg' ? ' hidden lg:table-cell' : '')
+        . ($responsive && $breakpoint === 'md' ? ' hidden md:table-cell' : '')
+        . ($responsive && $breakpoint === 'sm' ? ' hidden sm:table-cell' : '')
+        . ($lastOn === 'sm' ? ' last-cell last-cell-sm' : '')
+        . ($lastOn === 'md' ? ' last-cell last-cell-md' : '')
+        . ($lastOn === 'lg' ? ' last-cell last-cell-lg' : '')
+        . ($lastOn === 'xl' ? ' last-cell last-cell-xl' : '')
+        . ($firstOn === 'sm' ? ' first-cell first-cell-sm' : '')
+        . ($firstOn === 'md' ? ' first-cell first-cell-md' : '')
+        . ($firstOn === 'lg' ? ' first-cell first-cell-lg' : '')
+        . ($firstOn === 'xl' ? ' first-cell first-cell-xl' : '')
+        . ' ' . $class
+]) }}
+    @if ($colspan) colspan="{{ $colspan }}" @endif
+>
+    <div>
+        <div class="flex items-center space-x-2 font-semibold px-4 py-3 -mx-3 -my-4 {{ $colours }}">
+            <x-ark-status-circle-shallow :type="$type" />
+
+            <div>@lang('ui::status.'.$type)</div>
+        </div>
+    </div>
+</td>
--- src/UserInterfaceServiceProvider.php
+++ src/UserInterfaceServiceProvider.php
@@ -116,6 +116,7 @@ class UserInterfaceServiceProvider extends ServiceProvider
 
         Blade::component('ark::tables.row', 'ark-tables.row');
         Blade::component('ark::tables.cell', 'ark-tables.cell');
+        Blade::component('ark::tables.cell-status', 'ark-tables.cell-status');
         Blade::component('ark::tables.header', 'ark-tables.header');
         Blade::component('ark::tables.view-options', 'ark-tables.view-options');
 
