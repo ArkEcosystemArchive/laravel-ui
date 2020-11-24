07bcc912d83b3bb6c6c24164542a05dd94dc0315
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-24 09:51:42 +0000

    chore: add status langs

---
+++ resources/lang/en/status.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'success' => 'Success',
+    'failed'  => 'Failed',
+    'debug'   => 'Debug',
+    'running' => 'Running',
+    'updated' => 'Updated',
+    'active'  => 'Active',
+    'locked'  => 'Locked',
+];
