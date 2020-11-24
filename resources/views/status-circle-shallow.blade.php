a98639804a150662e92440c9662c684e8af1d80f
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-11-24 13:23:14 -0600

    Refactor icons view

--- resources/views/status-circle-shallow.blade.php
+++ resources/views/status-circle-shallow.blade.php
@@ -15,30 +15,28 @@
         '2xl'  => 'w-14 h-14',
         'base' => 'w-6 h-6',
     ][$containerSize ?? 'base'];
+
+    $icon = [
+        'success' => 'checkmark',
+        'failed' => 'cross',
+        'error' => 'cross',
+        'running' => 'update',
+        'paused' => 'update',
+        'updated' => 'update',
+        'locked' => 'lock',
+    ][$type];
+
+    $color = [
+        'success' => 'success-600',
+        'failed' => 'danger-400',
+        'error' => 'danger-400',
+        'running' => 'warning-900',
+        'paused' => 'warning-500',
+        'updated' => 'warning-900',
+        'locked' => 'secondary-700',
+    ][$type];
 @endphp
 
-@if ($type === 'success')
-    <div class="flex items-center justify-center flex-shrink-0 border-2 rounded-full border-theme-success-600 {{ $containerSize }}">
-        @svg('checkmark', 'text-theme-success-600 '.$size)
-    </div>
-@elseif ($type === 'failed' || $type === 'error')
-    <div class="flex items-center justify-center flex-shrink-0 border-2 rounded-full border-theme-danger-400 {{ $containerSize }}">
-        @svg('cross', 'text-theme-danger-400 '.$size)
-    </div>
-@elseif ($type === 'running')
-    <div class="flex items-center justify-center flex-shrink-0 border-2 rounded-full border-theme-warning-900 {{ $containerSize }}">
-        @svg('update', 'text-theme-warning-900 animation-spin '.$size)
-    </div>
-@elseif ($type === 'paused')
-    <div class="flex items-center justify-center flex-shrink-0 border-2 rounded-full border-theme-warning-500 {{ $containerSize }}">
-        @svg('update', 'text-theme-warning-500 '.$size)
-    </div>
-@elseif ($type === 'updated')
-    <div class="flex items-center justify-center flex-shrink-0 border-2 rounded-full border-theme-warning-900 {{ $containerSize }}">
-        @svg('update', 'text-theme-warning-900 '.$size)
-    </div>
-@elseif ($type === 'locked')
-    <div class="flex items-center justify-center flex-shrink-0 border-2 rounded-full border-theme-secondary-700 {{ $containerSize }}">
-        @svg('lock', 'text-theme-secondary-700 '.$size)
-    </div>
-@endif
+<div class="flex items-center justify-center flex-shrink-0 border-2 rounded-full border-theme-{{ $color }} {{ $containerSize }}">
+    @svg($icon, 'text-theme-'.$color.' '.$size)
+</div>
