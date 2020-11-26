14af72401f7e156682f7067c6fc1b9d870bd638d
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-11-26 08:46:51 -0600

    fix highlight on firefox issue (#115)

--- resources/views/tables/row.blade.php
+++ resources/views/tables/row.blade.php
@@ -2,6 +2,6 @@
     'danger' => false,
     'warning' => false,
 ])
-<tr {{ $attributes }} @if($danger) data-danger @elseif($warning) data-warning @endif>
+<tr {{ $attributes }} @if($danger) data-danger @elseif($warning) data-warning @endif class="h-full">
     {{ $slot }}
 </tr>
