6ea148d08fcc4d3d38fe7dcf6c37fa61f4acbcbd
alexbarnsley <alexbarnsley@users.noreply.github.com>
2020-11-27 12:41:31 +0000

    style: resolve style guide violations

--- resources/views/time.blade.php
+++ resources/views/time.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ actionOpen: false, value: '{{ $time ?? '00:00' }}' }" class="flex-1">
     <x-ark-dropdown
         wrapper-class="relative w-full"
-        button-class="form-select shadow-sm rounded w-full"
+        button-class="w-full rounded shadow-sm form-select"
         dropdown-classes="left-0 w-32 z-10"
         dropdown-property="actionOpen"
         :init-alpine="false"
@@ -10,13 +10,13 @@
             <div x-text="value" class="text-theme-secondary-900"></div>
         @endslot
 
-        <div class="items-center justify-center block text-theme-secondary-900 h-40 overflow-y-scroll dropdown-scrolling">
+        <div class="items-center justify-center block h-40 overflow-y-scroll text-theme-secondary-900 dropdown-scrolling">
             @for ($hour = 0; $hour < 24; $hour++)
                 @for ($minute = 0; $minute < 60; $minute += 30)
                     @php($value = str_pad($hour, 2, '0', STR_PAD_LEFT).':'.str_pad($minute, 2, '0', STR_PAD_LEFT))
 
                     <div
-                        class="cursor-pointer justify-center dropdown-entry py-2"
+                        class="justify-center py-2 cursor-pointer dropdown-entry"
                         @click="value='{{ $value }}'"
                     >
                         {{ $value }}
