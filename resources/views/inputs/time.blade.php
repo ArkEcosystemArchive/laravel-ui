50acbbe9df6761c7c6d2c081f7396c367ab599f0
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-27 13:03:51 +0000

    refactor: livewire & alpine value integration

--- resources/views/inputs/time.blade.php
+++ resources/views/inputs/time.blade.php
@@ -10,19 +10,24 @@
             <div x-text="value" class="text-theme-secondary-900"></div>
         @endslot
 
-        <div class="items-center justify-center block text-theme-secondary-900 h-40 overflow-y-scroll dropdown-scrolling">
+        <div
+            class="items-center justify-center block text-theme-secondary-900 h-40 overflow-y-scroll dropdown-scrolling"
+            wire:model="{{ $name ?? $id }}"
+        >
             @for ($hour = 0; $hour < 24; $hour++)
                 @for ($minute = 0; $minute < 60; $minute += 30)
                     @php($value = str_pad($hour, 2, '0', STR_PAD_LEFT).':'.str_pad($minute, 2, '0', STR_PAD_LEFT))
 
                     <div
                         class="cursor-pointer justify-center dropdown-entry py-2"
-                        @click="value='{{ $value }}'"
+                        @click="value = '{{ $value }}'; $dispatch('input', '{{ $value }}');"
                     >
                         {{ $value }}
                     </div>
                 @endfor
             @endfor
+
+            <input type="hidden" x-bind:value="value" />
         </div>
     </x-ark-dropdown>
 </div>
