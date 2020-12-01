2c7a7ea86299ec4758ebb7829d4d6555a4ff7b8f
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-01 12:00:12 -0600

    Forget to push the modal changes

--- resources/assets/js/modal.js
+++ resources/assets/js/modal.js
@@ -1,2 +1,24 @@
-import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
+const Modal = () => {
+    return {
+        init() {
+            const el = this.$el;
+
+            disableBodyScroll(el);
+
+            // Since alpine doesn't have any method to track the destruction of
+            // the element, this interval will ensure to enable the body
+            // scrolling once the modal is no longer visible
+            const interval = setInterval(() => {
+                if (['', 'none'].includes(window.getComputedStyle(el).display)) {
+                    enableBodyScroll(el);
+                    clearInterval(interval)
+                }
+            }, 500)
+        }
+    }
+}
+
+export default Modal;
 
