ea0149a5d417fcb2511f75d872a5b7b4f294981e
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-04 10:07:11 -0600

    Allow to change the social link size

--- resources/views/social-link.blade.php
+++ resources/views/social-link.blade.php
@@ -1,8 +1,14 @@
+@props([
+    'iconSize' => '4',
+    'url',
+    'icon',
+])
+
 <a
     href="{{ $url }}"
     target="_blank"
     rel="noopener noreferrer"
     class="transition-default hover:text-theme-secondary-500"
 >
-    @svg($icon, 'w-4 h-4')
+    @svg($icon, sprintf('w-%s h-%s', $iconSize, $iconSize))
 </a>
