83c983c127f2a2d470de2f869220a28ac2bc529c
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-07 09:27:34 -0600

    Rollback original icon

--- resources/views/social-link.blade.php
+++ resources/views/social-link.blade.php
@@ -1,14 +1,8 @@
-@props([
-    'iconSize' => '4',
-    'url',
-    'icon',
-])
-
 <a
     href="{{ $url }}"
     target="_blank"
     rel="noopener noreferrer"
     class="transition-default hover:text-theme-secondary-500"
 >
-    @svg($icon, sprintf('w-%s h-%s', $iconSize, $iconSize))
+    @svg($icon, 'w-4 h-4')
 </a>
