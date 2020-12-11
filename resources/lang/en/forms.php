40af1d9b87525f1d5e29e46fa6dc2bf5cb922255
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-11 10:36:25 -0600

    Markdown component

--- resources/lang/en/forms.php
+++ resources/lang/en/forms.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'attachment_pdf' => 'Attachment (.PDF)',
-    'email'          => 'E-Mail Address',
-    'message'        => 'Message',
-    'name'           => 'Name',
-    'subject'        => 'Subject',
+    'attachment_pdf'                => 'Attachment (.PDF)',
+    'email'                         => 'E-Mail Address',
+    'message'                       => 'Message',
+    'name'                          => 'Name',
+    'subject'                       => 'Subject',
+    'github_markdown_can_be_used'   => '* Github flavored markdown can be used',
 ];
