6663be9dbe8289d027e6e933c93047da3bc162e2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-12-14 10:14:44 +0000

    refactor: revert defaults

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -77,16 +77,16 @@
             @else
                 breakpoints: {
                     375: {
-                        slidesPerGroup: {{ $columns - 3 > 0 ? $columns - 3 : 1 }},
-                        slidesPerView: {{ $columns - 3 > 0 ? $columns - 3 : 1 }},
+                        slidesPerGroup: {{ $columns - 3 > 0 ? $columns - 3 : 2 }},
+                        slidesPerView: {{ $columns - 3 > 0 ? $columns - 3 : 2 }},
                     },
                     640: {
-                        slidesPerGroup: {{ $columns - 2 > 0 ? $columns - 2 : 2 }},
-                        slidesPerView: {{ $columns - 2 > 0 ? $columns - 2 : 2 }}
+                        slidesPerGroup: {{ $columns - 2 > 0 ? $columns - 2 : 3 }},
+                        slidesPerView: {{ $columns - 2 > 0 ? $columns - 2 : 3 }}
                     },
                     1024: {
-                        slidesPerGroup: {{ $columns - 1 > 0 ? $columns - 1 : 3 }},
-                        slidesPerView: {{ $columns - 1 > 0 ? $columns - 1 : 3 }}
+                        slidesPerGroup: {{ $columns - 1 > 0 ? $columns - 1 : 4 }},
+                        slidesPerView: {{ $columns - 1 > 0 ? $columns - 1 : 4 }}
                     },
                     1280: {
                         slidesPerGroup: {{ $columns }},
