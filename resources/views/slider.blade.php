5c73e0d115977fd573c71a55f7f58838e91a4304
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-12-14 10:20:36 +0000

    fix: mobile slider breakpoints (#161)

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -1,23 +1,42 @@
+@props([
+    'id',
+    'title'           => null,
+    'titleClass'      => 'text-2xl',
+    'titleTooltip'    => null,
+    'viewAllUrl'      => null,
+    'viewAllClass'    => '',
+    'hideNavigation'  => false,
+    'topPagination'   => false,
+    'paginationClass' => '',
+    'rows'            => 1,
+    'columns'         => 5,
+    'breakpoints'     => null,
+    'spaceBetween'    => 0,
+    'loop'            => false,
+    'allowTouch'      => true,
+])
+
 <div class="w-full">
-    @if ($title ?? $viewAllUrl ?? false)
+    @if ($title ?? $viewAllUrl)
         <div class="flex mb-4">
-            @if ($title ?? false)
+            @if ($title)
                 <div
-                    class="flex-1 relative {{ $titleClass ?? 'text-2xl' }} @if (!isset($viewAllUrl)) mb-5 @endif"
+                    class="flex-1 relative {{ $titleClass }} @if (!isset($viewAllUrl)) mb-5 @endif"
                 >
                     {{ $title }}
 
-                    @if ($titleTooltip ?? false)
+                    @if ($titleTooltip)
                         <x-ark-info :tooltip="$titleTooltip" class="absolute -top-10 ml-2" />
                     @endif
                 </div>
             @endif
 
-            @if ($viewAllUrl ?? false)
-                <div class="flex {{ $viewAllClass ?? '' }}">
+            @if ($viewAllUrl)
+                <div class="flex {{ $viewAllClass }}">
                     <div class="flex-1 my-auto text-sm text-right">
                         <a href="{{ $viewAllUrl }}" class="link">
                             @lang('ui::actions.view_all')
+
                             <x-ark-icon class="inline-block" name="chevron-right" size="2xs" />
                         </a>
                     </div>
@@ -27,24 +46,24 @@
     @endif
 
     <!-- Swiper -->
-    <div class="relative @if(!isset($hideNavigation)) px-10 @endif">
-        <div id="swiper-{{ $id }}" class="swiper-container @if ($rows ?? false) slider-multirow @endif">
-            @if(isset($topPagination))
-                <div class="swiper-pagination text-right mb-3 {{ $paginationClass ?? '' }}"></div>
+    <div class="relative @unless($hideNavigation) px-10 @endunless">
+        <div id="swiper-{{ $id }}" class="swiper-container @if ($rows > 1) slider-multirow @endif">
+            @if($topPagination)
+                <div class="swiper-pagination text-right mb-3 {{ $paginationClass }}"></div>
             @endif
 
             <div class="swiper-wrapper">
                 {{ $slot }}
             </div>
 
-            @if(!isset($topPagination))
-                <div class="swiper-pagination {{ $paginationClass ?? '' }}"></div>
-            @endif
+            @unless($topPagination)
+                <div class="swiper-pagination {{ $paginationClass }}"></div>
+            @endunless
         </div>
-        @if (!isset($hideNavigation))
+        @unless ($hideNavigation)
             <div class="swiper-{{ $id }}-pagination swiper-button-next outline-none"></div>
             <div class="swiper-{{ $id }}-pagination swiper-button-prev outline-none"></div>
-        @endif
+        @endunless
     </div>
 
     <script>
@@ -52,51 +71,51 @@
             slidesPerView: 1,
             slidesPerGroup: 1,
             slidesPerColumn: 1,
-            spaceBetween: {{ isset($spaceBetween) ? $spaceBetween : 0 }},
-            @if($breakpoints ?? false)
+            spaceBetween: {{ $spaceBetween }},
+            @if($breakpoints)
                 breakpoints: {{ $breakpoints }},
             @else
                 breakpoints: {
-                    @if (!isset($columns) || $columns > 1)
+                    @if ($columns > 1)
                     375: {
-                        slidesPerGroup: {{ ($columns ?? 5) - 3 }},
-                        slidesPerView: {{ ($columns ?? 5) - 3 }},
+                        slidesPerGroup: {{ $columns - 3 > 0 ? $columns - 3 : 2 }},
+                        slidesPerView: {{ $columns - 3 > 0 ? $columns - 3 : 2 }},
                     },
                     640: {
-                        slidesPerGroup: {{ ($columns ?? 5) - 2 }},
-                        slidesPerView: {{ ($columns ?? 5) - 2 }}
+                        slidesPerGroup: {{ $columns - 2 > 0 ? $columns - 2 : 3 }},
+                        slidesPerView: {{ $columns - 2 > 0 ? $columns - 2 : 3 }}
                     },
                     1024: {
-                        slidesPerGroup: {{ ($columns ?? 5) - 1 }},
-                        slidesPerView: {{ ($columns ?? 5) - 1 }}
+                        slidesPerGroup: {{ $columns - 1 > 0 ? $columns - 1 : 4 }},
+                        slidesPerView: {{ $columns - 1 > 0 ? $columns - 1 : 4 }}
                     },
                     1280: {
                     @else
                     1024: {
                     @endif
-                        slidesPerGroup: {{ $columns ?? 5 }},
-                        slidesPerView: {{ $columns ?? 5 }},
-                        @if ($rows ?? false)
-                        slidesPerColumn: {{ $rows }},
-                        slidesPerColumnFill: 'row',
+                        slidesPerGroup: {{ $columns }},
+                        slidesPerView: {{ $columns }},
+                        @if ($rows > 1)
+                            slidesPerColumn: {{ $rows }},
+                            slidesPerColumnFill: 'row',
                         @endif
                     },
                 },
             @endif
-            loop: {{ $loop ?? 'false' }},
+            loop: {{ $loop ? 'true' : 'false' }},
             loopFillGroupWithBlank: true,
             pagination: {
                 el: '#swiper-{{ $id }} .swiper-pagination',
-                clickable: {{ isset($hideNavigation) && !isset($topPagination) ? 'false' : 'true' }}
+                clickable: {{ $hideNavigation && !$topPagination ? 'false' : 'true' }}
             },
-            @if (!isset($hideNavigation))
+            @unless ($hideNavigation)
             navigation: {
                 nextEl: '.swiper-{{ $id }}-pagination.swiper-button-next',
                 prevEl: '.swiper-{{ $id }}-pagination.swiper-button-prev'
             },
-            @endif
+            @endunless
             watchOverflow: true,
-            allowTouchMove: {{ $allowTouch ?? true }}
+            allowTouchMove: {{ $allowTouch ? 'true' : 'false' }}
         });
     </script>
 </div>
