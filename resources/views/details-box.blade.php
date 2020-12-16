26eb56bf4306a64b57d12cb92090fcd1037de989
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-12-16 08:52:23 +0000

    refactor: details box props & allow class (#168)

--- resources/views/details-box.blade.php
+++ resources/views/details-box.blade.php
@@ -1,43 +1,59 @@
-<div class="flex items-center detail-box @if ($reverse ?? false) flex-row-reverse @endif">
-    @isset($iconWrapper)
+@props([
+    'title',
+    'class' => '',
+    'reverse' => false,
+    'iconWrapper' => null,
+    'iconWrapperClass' => '',
+    'iconTextClass' => '',
+    'iconClass' => '',
+    'icon' => null,
+    'iconRaw' => null,
+    'noBorder' => false,
+    'shallow' => false,
+    'tooltip' => null,
+])
+
+<div class="flex items-center detail-box {{ $class }} @if ($reverse) flex-row-reverse @endif">
+    @if ($iconWrapper)
         {{ $iconWrapper }}
-    @elseif(isset($icon) || isset($iconRaw))
-        @isset($noBorder)
-            <div class="flex-shrink-0 text-theme-secondary-900 dark:text-theme-secondary-600 {{ $iconWrapperClass ?? '' }} @unless ($reverse ?? false) mr-5 @else ml-5 @endif">
-                @if ($icon ?? false)
+    @elseif ($icon || $iconRaw)
+        @if ($noBorder)
+            <div class="flex-shrink-0 text-theme-secondary-900 dark:text-theme-secondary-600 {{ $iconWrapperClass }} @unless ($reverse) mr-5 @else ml-5 @endif">
+                @if ($icon)
                     <x-ark-icon :name="$icon" />
                 @else
                     {{ $iconRaw }}
                 @endif
             </div>
-        @elseif(isset($shallow))
-            <div class="flex-shrink-0 circled-icon text-theme-secondary-900 dark:text-theme-secondary-600 border-theme-secondary-900 dark:border-theme-secondary-600 {{ $iconWrapperClass ?? '' }} @unless ($reverse ?? false) mr-5 @else ml-5 @endif">
-                @if ($icon ?? false)
+        @elseif ($shallow)
+            <div class="flex-shrink-0 circled-icon text-theme-secondary-900 dark:text-theme-secondary-600 border-theme-secondary-900 dark:border-theme-secondary-600 {{ $iconWrapperClass }} @unless ($reverse) mr-5 @else ml-5 @endif">
+                @if ($icon)
                     <x-ark-icon :name="$icon" />
                 @else
                     {{ $iconRaw }}
                 @endif
             </div>
         @else
-            <div class="flex items-center justify-center p-2 rounded-full h-12 w-12 flex-shrink-0 bg-theme-secondary-200 dark:bg-theme-secondary-800 {{ $iconWrapperClass ?? '' }} @unless ($reverse ?? false) mr-5 @else ml-5 @endif">
-                @if ($icon ?? false)
-                    <x-ark-icon :name="$icon" :class="($iconTextClass ?? ' ') . ' ' . ($iconClass ?? '')" />
+            <div class="flex items-center justify-center p-2 rounded-full h-12 w-12 flex-shrink-0 bg-theme-secondary-200 dark:bg-theme-secondary-800 {{ $iconWrapperClass }} @unless ($reverse) mr-5 @else ml-5 @endif">
+                @if ($icon)
+                    <x-ark-icon :name="$icon" :class="$iconTextClass.' '.$iconClass" />
                 @else
                     {{ $iconRaw }}
                 @endif
             </div>
-        @endisset
+        @endif
     @endif
-
 
     <div class="flex flex-col space-y-2">
         <span class="text-sm font-semibold text-theme-secondary-500 dark:text-theme-secondary-700">
             {{ $title }}
         </span>
 
-        <div @if ($tooltip ?? false) data-tippy-content="{{ $tooltip }}" @endif>
+        <div @if ($tooltip) data-tippy-content="{{ $tooltip }}" @endif>
             @if((string) $slot === "")
-                <span class="font-semibold text-theme-secondary-700 dark:text-theme-secondary-200">@lang('generic.not_specified')</span>
+                <span class="font-semibold text-theme-secondary-700 dark:text-theme-secondary-200">
+                    @lang('generic.not_specified')
+                </span>
             @else
                 <span class="font-semibold text-theme-secondary-700 dark:text-theme-secondary-200">
                     {{ $slot }}
