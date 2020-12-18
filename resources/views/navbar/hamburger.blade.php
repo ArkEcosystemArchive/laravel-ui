62729f9af5ca3a4a9030ac21f3080edf6bec5323
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-12-18 16:52:50 +0000

    fix: navbar hamburger breakpoint property

--- resources/views/navbar/hamburger.blade.php
+++ resources/views/navbar/hamburger.blade.php
@@ -1,4 +1,18 @@
-<div class="flex items-center pr-6 border-r border-theme-secondary-300 md:hidden">
+@props([
+    'breakpoint' => 'md',
+])
+
+@php
+    // Exact class strings required to prevent purging
+    $breakpointClass = [
+        'sm' => 'sm:hidden',
+        'md' => 'md:hidden',
+        'lg' => 'lg:hidden',
+        'xl' => 'xl:hidden',
+    ][$breakpoint];
+@endphp
+
+<div class="flex items-center pr-6 border-r border-theme-secondary-300 {{ $breakpointClass }}">
     <button @click="open = !open" class="inline-flex justify-center items-center p-2 rounded-md transition duration-150 ease-in-out text-theme-secondary-900">
         <span :class="{ 'hidden': open, 'inline-flex': !open }">
             <x-ark-icon name="menu" size="sm" />
