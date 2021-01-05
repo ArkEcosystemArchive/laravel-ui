3e0ed220db965fac7f33c7bb2d2f65355c5bdf8e
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-01-05 16:46:25 -0600

    WIP

--- resources/views/inputs/markdown.blade.php
+++ resources/views/inputs/markdown.blade.php
@@ -1,29 +1,42 @@
 @props([
+    'xData' => '{}',
     'name',
-    'class' => null,
-    'label' => null,
     'id' => null,
     'model' => null,
-    'rows' => '10',
-    'placeholder' => null,
-    'readonly' => null,
-    'message' => null,
-    'required' => false,
-    'slot' => null,
 ])
 
-<div>
-    <x-ark-textarea
-        :class="$class"
-        :id="$id"
-        :name="$name"
-        :label="$label"
-        :rows="$rows"
-        :model="$model"
-        :placeholder="$placeholder"
-        :readonly="$readonly"
-        :message="$message"
-        :required="$required"
-    >{{ $slot ?? '' }}</x-ark-textarea>
-    <span class="text-sm themetext-sm text-theme-secondary-500">{{ trans('ui::forms.github_markdown_can_be_used') }}</span>
+@php
+$icons = [
+    'iconBold' => 'text-bold',
+    'iconItalic' => 'text-italic',
+    'iconStrike' => 'text-strike-through',
+    'iconUnderline' => 'text-underline',
+]
+@endphp
+<div
+    x-data="MarkdownEditor({{ $xData }})"
+    x-init="init"
+>
+    <div>
+        @for($i=1; $i<=6; $i++)
+        <template x-ref="iconH{{ $i }}">
+            @svg('wyswyg.H' . $i, 'inline h-5')
+        </template>
+        @endfor
+        @foreach($icons as $ref => $iconName)
+        <template x-ref="{{ $ref }}">
+            @svg('wyswyg.' . $iconName, 'inline h-5')
+        </template>
+        @endforeach
+    </div>
+
+    <input
+        x-ref="input"
+        type="hidden"
+        id="{{ $id ? $id : $name }}"
+        name="{{ $name }}"
+        wire:model="{{ $model ? $model : $name }}"
+    />
+
+    <div wire:ignore x-ref="editor"></div>
 </div>
