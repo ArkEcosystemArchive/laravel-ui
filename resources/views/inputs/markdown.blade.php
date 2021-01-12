8f1340c4b768867567c663efed25bd294b065c4e
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-01-12 13:15:21 -0600

    Add missing attributes on the editor

--- resources/views/inputs/markdown.blade.php
+++ resources/views/inputs/markdown.blade.php
@@ -3,6 +3,11 @@
     'name',
     'id' => null,
     'model' => null,
+    'class' => '',
+    'label' => '',
+    'height' => null,
+    'toolbarItems' => null,
+    'plugins' => null,
 ])
 
 @php
@@ -27,38 +32,59 @@ $icons = [
     'iconPreview' => 'monitor',
 ]
 @endphp
-<div
-    x-data="MarkdownEditor({{ $xData }})"
-    x-init="init"
-    class="overflow-hidden bg-white border-2 rounded border-theme-secondary-200"
->
-    <div x-show="showOverlay" class="fixed top-0 left-0 z-50 w-full h-full bg-black bg-opacity-75" style="display: none"></div>
-    <div>
-        @for($i=1; $i<=6; $i++)
-        <template x-ref="iconH{{ $i }}">
-            @svg('wyswyg.H' . $i, 'inline h-5')
-        </template>
-        @endfor
-        @foreach($icons as $ref => $iconName)
-        <template x-ref="{{ $ref }}">
-            @svg('wyswyg.' . $iconName, 'inline h-4')
-        </template>
-        @endforeach
-    </div>
 
-    <input
-        x-ref="input"
-        type="hidden"
-        id="{{ $id ? $id : $name }}"
-        name="{{ $name }}"
-        wire:model="{{ $model ? $model : $name }}"
-    />
+<div class="{{ $class ?? '' }}">
+    <div class="input-group">
+        <label for="{{ $id ?? $name }}" class="input-label @error($name) input-label--error @enderror">
+            {{ ($label ?? '') ? $label : trans('forms.' . $name) }}
+        </label>
 
-    <div wire:ignore x-ref="editor"></div>
+        <div class="input-wrapper">
+            <div
+                x-data="MarkdownEditor(
+                    @if($height)'{{ $height }}'@else null @endif,
+                    @if($toolbarItems){{ json_encode($toolbarItems) }}@else null @endif,
+                    @if($plugins){{ json_encode($plugins) }}@else null @endif,
+                    {{ $xData }}
+                )"
+                x-init="init"
+                class="overflow-hidden bg-white border-2 rounded border-theme-secondary-200"
+            >
+                <div x-show="showOverlay" class="fixed top-0 left-0 z-50 w-full h-full bg-black bg-opacity-75" style="display: none"></div>
+                <div>
+                    @for($i=1; $i<=6; $i++)
+                    <template x-ref="iconH{{ $i }}">
+                        @svg('wyswyg.H' . $i, 'inline h-5')
+                    </template>
+                    @endfor
+                    @foreach($icons as $ref => $iconName)
+                    <template x-ref="{{ $ref }}">
+                        @svg('wyswyg.' . $iconName, 'inline h-4')
+                    </template>
+                    @endforeach
+                </div>
 
-    <div x-cloak class="flex justify-end py-3 text-xs bg-white">
-        <span class="px-4 ">Words: <strong x-text="wordsCount"></strong></span>
-        <span class="px-4 border-l-2 border-theme-secondary-200">Characters: <strong x-text="charsCount"></strong></span>
-        <span class="px-4 border-l-2 border-theme-secondary-200">Reading time: <strong><span x-text="readMinutes"></span> min</strong></span>
+                <input
+                    x-ref="input"
+                    type="hidden"
+                    id="{{ $id ? $id : $name }}"
+                    name="{{ $name }}"
+                    wire:model="{{ $model ? $model : $name }}"
+                />
+
+                <div wire:ignore x-ref="editor"></div>
+
+                <div x-cloak class="flex justify-end py-3 text-xs bg-white">
+                    <span class="px-4 ">Words: <strong x-text="wordsCount"></strong></span>
+                    <span class="px-4 border-l-2 border-theme-secondary-200">Characters: <strong x-text="charsCount"></strong></span>
+                    <span class="px-4 border-l-2 border-theme-secondary-200">Reading time: <strong><span x-text="readMinutes"></span> min</strong></span>
+                </div>
+            </div>
+        </div>
+
+        @error($name)
+            <p class="input-help--error">{{ $message }}</p>
+        @enderror
     </div>
 </div>
+
