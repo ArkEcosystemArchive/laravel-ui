713555b5475c309146b3bedd9677dfd060c0f375
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-15 13:42:28 +0000

    refactor: move loading spinner rounded class

--- resources/views/loading-spinner.blade.php
+++ resources/views/loading-spinner.blade.php
@@ -1,9 +1,9 @@
 @props([
-    'class'             => '',
+    'class'             => 'rounded',
     'dimensions'        => 'w-48 h-48',
     'spinnerDimensions' => 'w-20 h-20',
 ])
 
-<div class="cursor-pointer flex items-center justify-center rounded absolute top-0 opacity-90 transition-default bg-theme-secondary-900 {{ $dimensions }} {{ $class }}">
+<div class="cursor-pointer flex items-center justify-center absolute top-0 opacity-90 transition-default bg-theme-secondary-900 {{ $dimensions }} {{ $class }}">
     @svg('oval', $spinnerDimensions.' text-white')
 </div>
