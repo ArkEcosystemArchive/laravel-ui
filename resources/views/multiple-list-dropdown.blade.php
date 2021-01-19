989f5b69fc6def3b5ba0deb104c58b7bebe94cc6
alexbarnsley <alexbarnsley@users.noreply.github.com>
2021-01-19 20:51:48 +0000

    style: resolve style guide violations

--- resources/views/multiple-list-dropdown.blade.php
+++ resources/views/multiple-list-dropdown.blade.php
@@ -12,14 +12,14 @@
         @lang('ui::general.multiple')
     </div>
 
-    <div class="absolute rounded-lg bg-white shadow-xl font-semibold p-8 mt-4 space-y-6" x-bind:class="{ hidden: ! showDropdown }">
+    <div class="absolute p-8 mt-4 space-y-6 font-semibold bg-white rounded-lg shadow-xl" x-bind:class="{ hidden: ! showDropdown }">
         @if ($description)
             <div class="text-theme-secondary-500">{{ $description }}</div>
         @endif
 
-        <div class="grid grid-cols-4 gap-x-8 gap-y-4">
+        <div class="grid grid-cols-4 gap-y-4 gap-x-8">
             @foreach ($values as $value)
-                <div class="flex space-x-2 items-center">
+                <div class="flex items-center space-x-2">
                     <x-ark-icon name="checkmark-thin" class="text-theme-success-600" size="sm" />
 
                     <div>{{ $value }}</div>
