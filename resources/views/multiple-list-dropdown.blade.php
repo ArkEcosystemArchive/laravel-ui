84cf3fbbb8b54f153917b13a2144e7572e3799ef
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-19 21:04:16 +0000

    refactor: make responsive & move css

--- resources/views/multiple-list-dropdown.blade.php
+++ resources/views/multiple-list-dropdown.blade.php
@@ -12,12 +12,12 @@
         @lang('ui::general.multiple')
     </div>
 
-    <div class="absolute rounded-lg bg-white shadow-xl font-semibold p-8 mt-4 space-y-6" x-bind:class="{ hidden: ! showDropdown }">
+    <div class="multiple-list-dropdown" x-bind:class="{ hidden: ! showDropdown }">
         @if ($description)
             <div class="text-theme-secondary-500">{{ $description }}</div>
         @endif
 
-        <div class="grid grid-cols-4 gap-x-8 gap-y-4">
+        <div class="multiple-list-grid">
             @foreach ($values as $value)
                 <div class="flex space-x-2 items-center">
                     <x-ark-icon name="checkmark-thin" class="text-theme-success-600" size="sm" />
