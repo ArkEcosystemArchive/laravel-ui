869c838a02f00352ce142e2f31d9f80a8afaa455
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-21 13:44:04 +0000

    refactor: rename single image component & convert to trait

---
+++ src/Components/UploadImageSingle.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\UserInterface\Components;
+
+use Livewire\WithFileUploads;
+
+trait UploadImageSingle
+{
+    use WithFileUploads;
+
+    public $imageSingle;
+
+    public array $imageSingleValidators = ['mimes:jpeg,png,bmp,jpg', 'max:2048'];
+
+    abstract public function render();
+
+    abstract public function updatedImageSingle();
+
+    abstract public function deleteImageSingle();
+}
