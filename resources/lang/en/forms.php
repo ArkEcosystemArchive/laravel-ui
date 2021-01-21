108ab7b4f1a7423dd9c768afa1b424ebb2491571
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-21 12:01:50 +0000

    chore: add upload image collection langs

--- resources/lang/en/forms.php
+++ resources/lang/en/forms.php
@@ -14,4 +14,10 @@ return [
         'min_size'     => 'Min :0 x :1',
         'max_filesize' => 'Max filesize :0',
     ],
+
+    'upload-image-collection' => [
+        'drag_drop_browse' => '<span class="hidden md:inline">Drag & Drop your files here or</span> <a @click="select()" class="link">Browse Files</a>',
+        'delete_image'     => 'Delete Image',
+        'requirements'     => '<span>Min size :width x :height. Max filesize :filesize.</span> <span>Max :quantity images</span>',
+    ],
 ];
