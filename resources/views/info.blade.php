fd91225244c23b986f5eeb9c972e8ca580aee51b
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-01-22 11:52:37 +0100

    refactor: info component options (#214)

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,6 +1,17 @@
+@props([
+    'class' => '',
+    'tooltip' => '',
+    'type' => 'question',
+    'large' => false
+])
+
 <div
     data-tippy-content="{{ $tooltip }}"
-    class="inline-block px-1 py-1 rounded-full bg-theme-primary-100 text-theme-primary-600 dark:bg-theme-secondary-800 dark:text-theme-secondary-600 {{ $class ?? '' }}"
+    class="inline-block cursor-pointer {{ $large ? 'p-1.5' : 'p-1' }} transition-default rounded-full bg-theme-primary-100 text-theme-primary-600 dark:bg-theme-secondary-800 dark:text-theme-secondary-600 hover:text-white hover:bg-theme-primary-700 dark:hover:text-theme-secondary-800 dark:hover:bg-theme-secondary-600 {{ $class }}"
 >
-    <x-ark-icon name="question-mark" size="2xs" />
+    @if($type === 'question')
+        <x-ark-icon name="question-mark" size="{{ $large ? 'xs' : '2xs' }}" />
+    @else
+        <x-ark-icon name="hint" size="{{ $large ? 'xs' : '2xs' }}" />
+    @endif
 </div>
