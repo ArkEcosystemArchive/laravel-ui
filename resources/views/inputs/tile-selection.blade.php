8691512b0ba4f779e093b05401ea0d8f237b1b06
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-22 01:20:10 +0000

    fix: tile options select all showing for single value

--- resources/views/inputs/tile-selection.blade.php
+++ resources/views/inputs/tile-selection.blade.php
@@ -39,7 +39,7 @@
             @endif
         </div>
 
-        @unless ($hiddenOptions || $single === false)
+        @unless ($hiddenOptions || $single === true)
             <label class="tile-selection-select-all">
                 <input
                     type="checkbox"
