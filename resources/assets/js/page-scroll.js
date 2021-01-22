9fd08d897d954cdbb22c4e9ef124d47e70b292fb
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-22 18:21:29 +0000

    fix: pagination scroll top when no navbar

--- resources/assets/js/page-scroll.js
+++ resources/assets/js/page-scroll.js
@@ -2,7 +2,7 @@ window.scrollToQuery = (selector) => {
     const navbar = document.querySelector("#navbar");
 
     window.scrollTo({
-        top: document.querySelector(selector).offsetTop - navbar.clientHeight,
+        top: document.querySelector(selector).offsetTop - (navbar ? navbar.clientHeight : 0),
         behavior: "smooth",
     });
 };
