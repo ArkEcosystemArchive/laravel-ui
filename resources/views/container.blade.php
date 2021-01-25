76bf4e1821a6c54da23cbb8c89398ec77d96fab2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-25 13:17:23 +0000

    refactor: project registration form tweaks (#217)

--- resources/views/container.blade.php
+++ resources/views/container.blade.php
@@ -5,7 +5,7 @@
 ])
 
 <div class="@if ($border) border-t-20 border-theme-secondary-100 @endif {{ $wrapperClass }}">
-    <div class="py-16 content-container {{ $containerClass }}">
+    <div class="py-16 content-container {{ $containerClass }}" {{ $attributes->only('id') }}>
         {{ $slot }}
     </div>
 </div>
