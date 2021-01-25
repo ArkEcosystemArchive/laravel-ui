341c82ba74caf6a1d27af5d0ce418b97f29b1daa
Jolan <mc-somango@live.fr>
2021-01-25 14:54:19 +0100

    refactor: replace avatar JS generation with PHP one (#219)

--- resources/views/avatar.blade.php
+++ resources/views/avatar.blade.php
@@ -1,7 +1,5 @@
-<div
-    x-data="{ avatarImage: '' }"
-    x-init="avatarImage = createAvatar('{{ $identifier }}')"
-    class="overflow-hidden {{ $class ?? 'w-12 h-12 rounded-lg md:h-16 md:w-16 md:rounded-xl' }}"
->
-    <div x-html="avatarImage" class="object-cover w-full h-full"></div>
+<div class="avatar-wrapper overflow-hidden {{ $class ?? 'w-12 h-12 rounded-lg md:h-16 md:w-16 md:rounded-xl' }}">
+    <div class="object-cover w-full h-full">
+        {!! ARKEcosystem\UserInterface\Support\Avatar::make($identifier) !!}
+    </div>
 </div>
