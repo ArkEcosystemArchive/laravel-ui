44cbfb95713ab8581502e3750638b4e3723e448c
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-01-26 09:16:11 -0600

    Update navbar height

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -19,8 +19,8 @@
     ></div>
 
     <nav class="relative z-30 bg-white shadow-header-smooth dark:shadow-none dark:bg-theme-secondary-900">
-        <div class="px-4 sm:px-6 lg:px-8">
-            <div class="flex relative justify-between h-20 md:h-24">
+        <div class="px-4 sm:px-6 lg:px-8 py-0.5">
+            <div class="relative flex justify-between h-20">
                 @include('ark::navbar.logo')
 
                 @isset($middle)
@@ -28,7 +28,7 @@
                 @endisset
 
                 <div class="flex justify-end">
-                    <div class="flex flex-1 justify-end items-center sm:items-stretch sm:justify-between">
+                    <div class="flex items-center justify-end flex-1 sm:items-stretch sm:justify-between">
                         @isset($desktop)
                             {{ $desktop }}
                         @else
@@ -36,7 +36,7 @@
                         @endisset
                     </div>
 
-                    <div class="flex inset-y-0 right-0 items-center pr-2 sm:static sm:inset-auto sm:ml-4 sm:pr-0">
+                    <div class="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-4 sm:pr-0">
                         @include('ark::navbar.hamburger')
 
                         @isset($content)
