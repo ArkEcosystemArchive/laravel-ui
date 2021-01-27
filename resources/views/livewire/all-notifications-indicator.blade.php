44451a9b56d8833f94c1d0ae53e93c329e39e2cd
alfonsobries <alfonsobries@users.noreply.github.com>
2021-01-27 21:39:12 +0000

    style: resolve style guide violations

--- resources/views/livewire/all-notifications-indicator.blade.php
+++ resources/views/livewire/all-notifications-indicator.blade.php
@@ -1,6 +1,6 @@
 <span>
     @if($show)
-        <span class="absolute right-0 block p-1 -mt-4 bg-white rounded-full md:hidden">
+        <span class="block absolute right-0 p-1 -mt-4 bg-white rounded-full md:hidden">
             <span class="block w-1 h-1 rounded-full border-3 bg-theme-danger-500 border-theme-danger-500"></span>
         </span>
     @endif
