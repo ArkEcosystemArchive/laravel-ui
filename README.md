636cfd0b19fbbdfd697f8a79f673a6230462e2cf
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-27 12:42:34 +0000

    refactor: icon viewbox fixes & markdown scripts component (#225)

--- README.md
+++ README.md
@@ -124,9 +124,7 @@ window.MarkdownEditor = MarkdownEditor;
 
 ```html
 @push('scripts')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.48.4/codemirror.min.css" />
-    <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js" defer></script>
-    <link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor-only.min.css" />
+    <x-ark-pages-includes-markdown-scripts />
 @endpush
 ```
 
--- src/UserInterfaceServiceProvider.php
+++ src/UserInterfaceServiceProvider.php
@@ -176,6 +176,8 @@ class UserInterfaceServiceProvider extends ServiceProvider
         Blade::component('ark::pages.contact.content', 'ark-pages-contact-content');
         Blade::component('ark::pages.contact.header', 'ark-pages-contact-header');
 
+        Blade::component('ark::pages.includes.markdown-scripts', 'ark-pages-includes-markdown-scripts');
+
         Blade::component('ark::tables.row', 'ark-tables.row');
         Blade::component('ark::tables.cell', 'ark-tables.cell');
         Blade::component('ark::tables.cell-status', 'ark-tables.cell-status');
