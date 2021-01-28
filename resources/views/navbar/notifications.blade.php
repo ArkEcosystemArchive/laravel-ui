c6d5c5dba15fcea445bcb2dd0296c9d2851484b8
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-01-28 12:35:58 -0600

    Add shadow and margin to the notifications dropdown

--- resources/views/navbar/notifications.blade.php
+++ resources/views/navbar/notifications.blade.php
@@ -1,5 +1,10 @@
 <div class="{{ $class ?? ''}}" @click="livewire.emit('markNotificationsAsSeen')">
-    <x-ark-dropdown wrapper-class="mx-1 md:relative" dropdown-classes="mt-4 {{ $dropdownClasses ?? '' }}" button-class="relative">
+    <x-ark-dropdown
+        wrapper-class="mx-1 md:relative"
+        dropdown-classes="mt-8 md:px-0 px-8 {{ $dropdownClasses ?? '' }}"
+        button-class="relative"
+        dropdown-content-classes="bg-white dark:bg-theme-secondary-800 dark:text-theme-secondary-200 rounded-xl shadow-2xl"
+    >
         <x-slot name="button">
             @svg('notification', 'h-5 w-5 transition-default text-theme-secondary-600 hover:text-theme-primary-700')
 
