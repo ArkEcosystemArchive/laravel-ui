247a560d7e88b23381a91363c1704de1a6d0a4fc
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-02-01 16:32:20 +0000

    refactor: accordion component changes (#236)

--- resources/views/accordion.blade.php
+++ resources/views/accordion.blade.php
@@ -1,28 +1,39 @@
-@php ($isDark = $dark ?? false)
+@props([
+    'title',
+    'slot',
+    'dark' => false,
+    'border' => true,
+    'containerClass' => 'p-6',
+    'titleClass' => '',
+    'circleClass' => '',
+    'circleSize' => 'sm',
+])
+
+@php ($isDark = $dark)
+
 <div x-data="{ openPanel: null }">
     <dl>
-        <div class="{{ $containerClass ?? 'p-6' }} @if($isDark === false) border-2 border-theme-secondary-200 rounded-lg @endif">
+        <div class="{{ $containerClass }} @if ($isDark === false && $border) border-2 border-theme-secondary-200 rounded-lg @endif">
             <dt>
-                <button
-                    type="button"
-                    class="text-left w-full flex justify-between items-center focus:outline-none
-                        @if($isDark) text-theme-secondary-400 @else text-theme-secondary-900 @endif"
+                <a
+                    class="text-left w-full flex justify-between items-center
+                        {{ $isDark ? 'text-theme-secondary-400' : 'text-theme-secondary-900' }}"
                     :class="{ 'mb-5': openPanel }"
                     @click="openPanel = (openPanel ? null : 1)"
                 >
-                    <span class="text-lg font-semibold">
+                    <span class="text-lg font-semibold {{ $titleClass }}">
                         {{ $title }}
                     </span>
 
                     <span class="flex items-center ml-6 h-7">
                         <span
                             :class="{ 'rotate-180 text-theme-primary-500': openPanel, 'text-theme-secondary-500': !openPanel }"
-                            class="transition duration-150 ease-in-out transform"
+                            class="transition duration-150 ease-in-out transform {{ $circleClass }}"
                         >
-                            @svg('chevron-down', 'h-4 w-4')
+                            <x-ark-icon name="chevron-down" :size="$circleSize" />
                         </span>
                     </span>
-                </button>
+                </a>
             </dt>
 
             <dd class="mt-2 @if($isDark) border-l border-theme-secondary-800 pl-8 @endif" x-show.transition.opacity="openPanel" x-cloak>
