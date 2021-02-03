f89a02f94928bfaa784deb6e7f129955aea2a306
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-02-03 15:41:13 +0000

    feat: add handle upload error trait concern

---
+++ src/Components/Concerns/HandleUploadError.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ARKEcosystem\UserInterface\Components\Concerns;
+
+trait HandleUploadError
+{
+    use HandleToast;
+
+    public function uploadError(?string $message = null): void
+    {
+        if (! $message) {
+            $message = trans('ui::forms.upload-image.upload_error');
+        }
+
+        $this->toast($message, 'error');
+    }
+}
