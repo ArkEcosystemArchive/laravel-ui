ab5b720b62348c32b07ea0ec127fbe1769435c14
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-02-05 10:46:16 -0600

    Add hover state to the navbar avatar

--- resources/views/navbar/profile.blade.php
+++ resources/views/navbar/profile.blade.php
@@ -4,16 +4,16 @@
     dusk="profile-dropdown"
 >
     <x-slot name="button">
-        <span class="inline-block relative avatar-wrapper">
+        <span class="relative inline-block avatar-wrapper">
             @isset($identifier)
                 <x-ark-avatar
                     :identifier="$identifier"
-                    class="w-10 h-10 rounded-lg border-2 border-transparent md:h-11 md:w-11"
+                    class="w-10 h-10 border-2 border-transparent rounded-lg md:h-11 md:w-11 hover:border-theme-primary-700"
                     x-bind:class="{ 'border-theme-primary-700': dropdownOpen }"
                 />
             @else
                 <div
-                    class="overflow-hidden w-12 h-12 rounded-lg md:h-16 md:w-16 md:rounded-xl"
+                    class="w-12 h-12 overflow-hidden rounded-lg md:h-16 md:w-16 md:rounded-xl"
                 >
                     <img class="object-cover w-full h-full" src="{{ $profilePhoto }}" alt="Profile Avatar" />
                 </div>
