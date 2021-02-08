dc8a3b9c8744056ddf5307925bbe0957a06f97f7
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-02-08 10:16:36 -0600

    Make contact page social icons configurable

--- resources/views/icon.blade.php
+++ resources/views/icon.blade.php
@@ -1,16 +1,26 @@
+@props([
+    'name',
+    'size' => 'base',
+    'style' => '',
+    'class' => ''
+])
 @php
-    $size = [
-        '2xs'  => 'w-2 h-2 ',
-        'xs'   => 'w-3 h-3 ',
-        'sm'   => 'w-4 h-4 ',
-        'md'   => 'w-6 h-6 ',
-        'lg'   => 'w-8 h-8 ',
-        'xl'   => 'w-12 h-12 ',
-        '2xl'  => 'w-14 h-14 ',
-        'base' => 'w-5 h-5 ',
-    ][$size ?? 'base'];
+    $availableSizes = ['2xs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', 'base'];
 
-    if (isset($style)) {
+    if (array_key_exists($size, $availableSizes) || empty($size)) {
+        $size = [
+            '2xs'  => 'w-2 h-2 ',
+            'xs'   => 'w-3 h-3 ',
+            'sm'   => 'w-4 h-4 ',
+            'md'   => 'w-6 h-6 ',
+            'lg'   => 'w-8 h-8 ',
+            'xl'   => 'w-12 h-12 ',
+            '2xl'  => 'w-14 h-14 ',
+            'base' => 'w-5 h-5 ',
+        ][$size ?? 'base'];
+    }
+
+    if (!empty($style)) {
         $style = [
             'secondary' => 'text-theme-secondary-500 ',
             'success'   => 'text-theme-success-500 ',
@@ -19,4 +29,4 @@
     }
 @endphp
 
-@svg($name, $size . ($style ?? '') . ($class ?? ''), ['wire:key' => Str::random(8)])
+@svg($name, $size . $style . $class, ['wire:key' => Str::random(8)])
--- resources/views/social-square.blade.php
+++ resources/views/social-square.blade.php
@@ -1,10 +1,19 @@
+@props([
+    'icon',
+    'url',
+    'internal' => false,
+    'hoverClass' => 'hover:bg-theme-danger-400 hover:text-white',
+])
+
 <a
     href="{{ $url }}"
-    class="block w-16 h-14 rounded-lg border cursor-pointer border-theme-secondary-300 lg:w-14 lg:h-12 transition-default hover:bg-theme-danger-400 hover:text-white"
-    @unless($internal ?? false)
+    class="block w-16 border rounded-lg cursor-pointer h-14 border-theme-secondary-300 lg:w-14 lg:h-12 transition-default {{ $hoverClass }}"
+    @unless($internal)
         target="_blank"
         rel="noopener noreferrer"
     @endunless
 >
-    <div class="flex justify-center items-center h-full">@svg($icon, 'w-6 h-6 lg:w-5 lg:h-5')</div>
+    <div class="flex items-center justify-center h-full">
+        <x-ark-icon :name="$icon" size="w-6 h-6 lg:w-5 lg:h-5" />
+    </div>
 </a>
