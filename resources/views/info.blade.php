9891b428530c56a0ffae181e76ab2ed1d1ecb6f8
Maurizio <maurizio.lepora@gmail.com>
2021-02-10 11:44:55 +0100

    refactor: info component

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,17 +1,53 @@
 @props([
-    'class' => '',
     'tooltip' => '',
     'type' => 'question',
     'large' => false
 ])
 
-<div
-    data-tippy-content="{{ $tooltip }}"
-    class="inline-block cursor-pointer {{ $large ? 'p-1.5' : 'p-1' }} transition-default rounded-full bg-theme-primary-100 text-theme-primary-600 dark:bg-theme-secondary-800 dark:text-theme-secondary-600 hover:text-white hover:bg-theme-primary-700 dark:hover:text-theme-secondary-800 dark:hover:bg-theme-secondary-600 {{ $class }}"
->
-    @if($type === 'question')
-        <x-ark-icon name="question-mark" size="{{ $large ? 'xs' : '2xs' }}" />
-    @else
-        <x-ark-icon name="hint" size="{{ $large ? 'xs' : '2xs' }}" />
-    @endif
+@php
+    $typeIcon = [
+        'info' => 'info',
+        'hint' => 'hint',
+        'question' => 'question-mark',
+        'warning' => 'question-mark',
+    ][$type];
+
+    $typeClasses = [
+        'info' => [
+            'bg-theme-primary-100', 'text-theme-primary-600',
+            'dark:bg-theme-secondary-800', 'dark:text-theme-secondary-600',
+            'hover:text-white', 'hover:bg-theme-primary-700',
+            'dark:hover:text-theme-secondary-800', 'dark:hover:bg-theme-secondary-600',
+        ],
+        'hint' => [
+            'bg-theme-primary-100', 'text-theme-primary-600',
+            'dark:bg-theme-secondary-800', 'dark:text-theme-secondary-600',
+            'hover:text-white', 'hover:bg-theme-primary-700',
+            'dark:hover:text-theme-secondary-800', 'dark:hover:bg-theme-secondary-600',
+        ],
+        'question' => [
+            'bg-theme-primary-100', 'text-theme-primary-600',
+            'dark:bg-theme-secondary-800', 'dark:text-theme-secondary-600',
+            'hover:text-white', 'hover:bg-theme-primary-700',
+            'dark:hover:text-theme-secondary-800', 'dark:hover:bg-theme-secondary-600',
+        ],
+        'warning' => [
+            'border-2', 'border-theme-warning-600', 'text-theme-warning-600',
+            'dark:border-theme-warning-100', 'dark:text-theme-warning-100',
+            'hover:border-theme-warning-600', 'hover:text-theme-warning-600',
+            'dark:hover:border-theme-warning-100', 'dark:hover:text-theme-warning-100',
+        ],
+    ][$type];
+
+    $classes = array_merge([
+        'inline-block',
+        'cursor-pointer',
+        $large ? 'p-1.5' : 'p-1',
+        'transition-default',
+        'rounded-full',
+], $typeClasses);
+@endphp
+
+<div data-tippy-content="{{ $tooltip }}" {{ $attributes->merge(['class' => implode(' ', $classes)]) }}>
+    <x-ark-icon :name="$typeIcon" size="{{ $large ? 'xs' : '2xs' }}" />
 </div>
