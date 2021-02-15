1dc3b97b76535b87760af64f7253db164c106b5d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-02-15 18:58:02 +0000

    refactor: container

--- resources/views/container.blade.php
+++ resources/views/container.blade.php
@@ -1,11 +1,9 @@
 @props([
-    'border' => false,
     'containerClass' => 'flex flex-col',
-    'wrapperClass' => '',
 ])
 
-<div class="@if ($border) border-t-20 border-theme-secondary-100 @endif {{ $wrapperClass }}">
-    <div class="py-8 w-full content-container {{ $containerClass }}" {{ $attributes->only('id') }}>
+<div {{ $attributes->except('containerClass') }}>
+    <div class="py-8 w-full content-container {{ $containerClass }}">
         {{ $slot }}
     </div>
 </div>
