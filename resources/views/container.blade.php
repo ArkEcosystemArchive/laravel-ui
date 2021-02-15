27f0a143908fe1d32b9c69e31fd31c919f9fb946
ItsANameToo <itsanametoo@protonmail.com>
2021-02-15 11:51:57 +0100

    refactor: decrease container padding

--- resources/views/container.blade.php
+++ resources/views/container.blade.php
@@ -5,7 +5,7 @@
 ])
 
 <div class="@if ($border) border-t-20 border-theme-secondary-100 @endif {{ $wrapperClass }}">
-    <div class="py-16 content-container {{ $containerClass }}" {{ $attributes->only('id') }}>
+    <div class="py-8 w-full content-container {{ $containerClass }}" {{ $attributes->only('id') }}>
         {{ $slot }}
     </div>
 </div>
