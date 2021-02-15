fbb0019adf5d8e3cb22cfb670da4aedfdcdf8cdd
Highjhacker <jolanbeer@gmail.com>
2021-02-15 11:00:47 +0100

    refactor: font size tweaks for alert component

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -1,3 +1,10 @@
+@props([
+    'message' => null,
+    'messageClass' => 'text-sm',
+    'type' => null,
+    'large' => false,
+])
+
 <div class="alert-wrapper alert-{{ $type }}">
     <div class="alert-icon-wrapper alert-{{ $type }}-icon @if($large ?? false) alert-icon-large @endif">
         @svg(in_array($type, ['success', 'error', 'danger', 'hint', 'warning', 'info']) ? 'alert-'.$type : 'alert-default', $large ?? '' ? 'h-10 w-10' : 'h-8 w-8')
@@ -6,9 +13,9 @@
     <div class="alert-content-wrapper alert-{{ $type }}-content @if($large ?? false) alert-content-large @endif">
         @isset($title)<span class="alert-{{ $type }}-title">{{ $title }}</span>@endif
         @isset($message)
-            <span class="block">{{ $message }}</span>
+            <span class="block {{ $messageClass }}">{{ $message }}</span>
         @else
-            <span class="block">{{ $slot }}</span>
+            <span class="block {{ $messageClass }}">{{ $slot }}</span>
         @endif
     </div>
 </div>
