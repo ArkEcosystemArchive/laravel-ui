a1b740b2ea83fe580d2a37786abe3465646c0d34
Maurizio <leMaur@users.noreply.github.com>
2021-02-15 17:20:07 +0100

    refactor: tooltip duration (#276)

--- resources/assets/js/tippy.js
+++ resources/assets/js/tippy.js
@@ -4,12 +4,14 @@ import "tippy.js/dist/tippy.css";
 /** Enable tooltips for components with this data attribute, and global config options */
 tippy("[data-tippy-content]", {
     trigger: "mouseenter focus",
+    duration: 0,
 });
 
 tippy("[data-tippy-hover]", {
     touch: "hold",
     trigger: "mouseenter",
     content: (reference) => reference.dataset.tippyHover,
+    duration: 0,
 });
 
 window.initClipboard = () => {
