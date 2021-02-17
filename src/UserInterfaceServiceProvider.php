206191e48aa3b8a83636f2c484b083f5c83be0bf
Jolan <mc-somango@live.fr>
2021-02-17 15:24:18 +0100

    fix: register file-download into package service provider (#284)

--- src/UserInterfaceServiceProvider.php
+++ src/UserInterfaceServiceProvider.php
@@ -245,6 +245,7 @@ class UserInterfaceServiceProvider extends ServiceProvider
         Blade::component('ark::shapes.line', 'ark-placeholder-line');
         Blade::component('ark::shapes.square', 'ark-placeholder-square');
         Blade::component('ark::link-collection', 'ark-link-collection');
+        Blade::component('ark::file-download', 'ark-file-download');
 
         // Navigation
         Blade::component('ark::navbar', 'ark-navbar');
