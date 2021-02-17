8fee1d07f4f0952bddf13ee5238dcea115a5eda8
Highjhacker <jolanbeer@gmail.com>
2021-02-17 14:11:46 +0100

    refactor: requested changes

--- resources/assets/js/file-download.js
+++ resources/assets/js/file-download.js
@@ -3,16 +3,12 @@ window.fileDownload = () => {
         save: function (filename, data, dataType, extension) {
             let blob = new Blob([data], { type: dataType });
 
-            if (window.navigator.msSaveOrOpenBlob) {
-                window.navigator.msSaveBlob(blob, `${filename}.${extension}`);
-            } else {
-                let elem = window.document.createElement('a');
-                elem.href = window.URL.createObjectURL(blob);
-                elem.download = `${filename}.${extension}`;
-                document.body.appendChild(elem);
-                elem.click();
-                document.body.removeChild(elem);
-            }
+            let elem = window.document.createElement('a');
+            elem.href = window.URL.createObjectURL(blob);
+            elem.download = `${filename}.${extension}`;
+            document.body.appendChild(elem);
+            elem.click();
+            document.body.removeChild(elem);
         },
     };
 };
