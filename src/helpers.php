2f3e8ed204fd40fac4716ec75d23a9c8d7b57507
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-02-19 14:35:11 +0000

    refactor: read time helper (#289)

--- src/helpers.php
+++ src/helpers.php
@@ -8,3 +8,16 @@ if (! function_exists('svgLazy')) {
         return new SvgLazy($name, $class);
     }
 }
+
+if (! function_exists('formatReadTime')) {
+    function formatReadTime(int $minutes): string
+    {
+        $readTime = (new DateTime())->setTime(0, $minutes);
+
+        return sprintf(
+            '%s %s',
+            trans_choice('generic.time.hour', (int) $readTime->format('H'), ['value' => (int) $readTime->format('H')]),
+            trans_choice('generic.time.min', (int) $readTime->format('i'), ['value' => (int) $readTime->format('i')])
+        );
+    }
+}
