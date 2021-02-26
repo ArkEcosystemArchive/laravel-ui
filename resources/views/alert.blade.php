6ce69b0d9c0cc2e5dd7417a4bd092acfb9d379ef
Highjhacker <jolanbeer@gmail.com>
2021-02-26 11:34:13 +0100

    refactor: alert component tweaks

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -3,17 +3,18 @@
     'messageClass' => 'text-sm',
     'type' => 'info',
     'large' => false,
+    'rounded' => false,
 ])
 
-<div class="alert-wrapper alert-{{ $type }}">
+<div class="alert-wrapper alert-{{ $type }} items-center">
     <div class="alert-icon-wrapper alert-{{ $type }}-icon @if($large) alert-icon-large @endif">
-        @svg(in_array($type, ['success', 'error', 'danger', 'hint', 'warning', 'info']) ? 'alert-'.$type : 'alert-default', $large ? 'h-10 w-10' : 'h-8 w-8')
+        @svg(alertIcon($type, $rounded), $large ? 'h-10 w-10' : 'h-8 w-8')
     </div>
 
     <div class="alert-content-wrapper alert-{{ $type }}-content @if($large) alert-content-large @endif">
         @isset($title)<span class="alert-{{ $type }}-title">{{ $title }}</span>@endif
         @isset($message)
-            <span class="block {{ $messageClass }}">{{ $message }}</span>
+            <span class="block leading-6 {{ $messageClass }}">{{ $message }}</span>
         @else
             <span class="block {{ $messageClass }}">{{ $slot }}</span>
         @endif
