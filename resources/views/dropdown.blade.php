766a1cc24dede8d00c996f09d23e759d1ef6d280
Maurizio <maurizio.lepora@gmail.com>
2021-02-26 09:54:41 +0100

    chore: add full-screen logic to dropdown component

--- resources/views/dropdown.blade.php
+++ resources/views/dropdown.blade.php
@@ -36,10 +36,10 @@
         x-transition:leave="transition ease-in duration-75"
         x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95"
-        class="origin-top-right absolute {{ $fullScreen ? 'left-0' : '' }} right-0 mt-2 z-10 dropdown {{ $dropdownClasses ?? 'w-40' }}"
+        class="origin-top-right absolute right-0 mt-2 z-10 dropdown {{ $dropdownClasses ?? 'w-40' }} {{ $fullScreen ? 'w-screen -mx-8 md:w-auto md:mx-0' : '' }}"
         @if ($height ?? false) data-height="{{ $height }}" @endif
     >
-        <div class="{{ $dropdownContentClasses ??  'w-full bg-white rounded-md shadow-lg dark:bg-theme-secondary-800 dark:text-theme-secondary-200' }}" x-cloak>
+        <div class="{{ $dropdownContentClasses ??  'bg-white rounded-md shadow-lg dark:bg-theme-secondary-800 dark:text-theme-secondary-200' }}" x-cloak>
             <div class="py-1" @if($closeOnClick ?? true) @click="{{ $dropdownProperty }} = !{{ $dropdownProperty }}" @endif>
                 {{ $slot }}
             </div>
