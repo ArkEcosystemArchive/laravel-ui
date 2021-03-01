d8021d0a8d088bd4c7d2de353a43c523a4f232f5
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-03-01 12:49:51 +0100

    refactor: tweak alerts to match new design (#319)

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -7,7 +7,9 @@
 
 <div class="alert-wrapper alert-{{ $type }} items-center">
     <div class="alert-icon-wrapper alert-{{ $type }}-icon @if($large) alert-icon-large @endif">
-        @svg(alertIcon($type), $large ? 'h-10 w-10' : 'h-8 w-8')
+        <div class="flex items-center justify-center border-2 border-white rounded-full {{ $large ? 'h-10 w-10' : 'h-6 w-6' }}">
+            @svg(alertIcon($type), $large ? 'h-6 w-6' : 'h-3 w-3')
+        </div>
     </div>
 
     <div class="alert-content-wrapper alert-{{ $type }}-content @if($large) alert-content-large @endif">
