e48df60fbe558ca596c7b54fe1d1fed34f0b0b5c
Jolan <mc-somango@live.fr>
2021-03-05 11:09:59 +0100

    fix: reset error bag after displaying upload error (#329)

--- src/Components/UploadImageSingle.php
+++ src/Components/UploadImageSingle.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ARKEcosystem\UserInterface\Components;
 
-use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Validator;
 use Livewire\WithFileUploads;
 use ARKEcosystem\UserInterface\Components\Concerns\HandleUploadError;
