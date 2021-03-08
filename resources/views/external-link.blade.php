3dedf13fa204b453f327ddbe6c04b168fbcbe5d4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-03-08 19:24:59 +0000

    refactor: use props

--- resources/views/external-link.blade.php
+++ resources/views/external-link.blade.php
@@ -1,7 +1,17 @@
+@props([
+    'url',
+    'text',
+    'class'     => 'link font-semibold',
+    'inline'    => false,
+    'allowWrap' => false,
+    'small'     => false,
+])
+
 <a
     href="{{ $url }}"
-    class="{{ $class ?? 'link font-semibold' }} {{ ($inline ?? false) ? 'inline space-x-1' : 'flex items-center space-x-2' }} {{ ($allowWrap ?? false) ? '' : 'whitespace-nowrap' }}"
-    target="_blank" rel="noopener nofollow noreferrer"
+    class="{{ $class }} {{ $inline ? 'inline space-x-1' : 'flex items-center space-x-2' }} {{ $allowWrap ? '' : 'whitespace-nowrap' }}"
+    target="_blank"
+    rel="noopener nofollow noreferrer"
 >
     <span>{{ $text }}</span>
 
