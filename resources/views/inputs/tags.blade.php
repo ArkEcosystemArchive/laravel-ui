28ca708380a0184905e15828872dd5fb8f7a6c93
alfonsobries <alfonsobries@users.noreply.github.com>
2021-03-08 15:47:31 +0000

    style: resolve style guide violations

--- resources/views/inputs/tags.blade.php
+++ resources/views/inputs/tags.blade.php
@@ -36,7 +36,7 @@
             <div
                 wire:ignore
                 x-ref="input"
-                class="relative px-3 py-2 bg-white border rounded border-theme-secondary-400"
+                class="relative py-2 px-3 bg-white rounded border border-theme-secondary-400"
             >
             </div>
 
