074c48c49ca9f2edb6741c49d33e7960d77fc338
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-03-09 10:29:55 +0000

    refactor: external url component props (#341)

--- resources/views/external-link.blade.php
+++ resources/views/external-link.blade.php
@@ -1,9 +1,23 @@
+@props([
+    'url',
+    'text',
+    'class'     => 'link font-semibold',
+    'inline'    => false,
+    'allowWrap' => false,
+    'small'     => false,
+])
+
 <a
     href="{{ $url }}"
-    class="{{ $class ?? 'link font-semibold' }} {{ ($inline ?? false) ? 'inline space-x-1' : 'flex items-center space-x-2' }} {{ ($allowWrap ?? false) ? '' : 'whitespace-nowrap' }}"
-    target="_blank" rel="noopener nofollow noreferrer"
+    class="{{ $class }} {{ $inline ? 'inline space-x-1' : 'flex items-center space-x-2' }} {{ $allowWrap ? '' : 'whitespace-nowrap' }}"
+    target="_blank"
+    rel="noopener nofollow noreferrer"
 >
     <span>{{ $text }}</span>
 
-    @svg('link', (($small ?? false) ? 'h-3 w-3' : 'h-4 w-4').' flex-shrink-0 '.(($inline ?? false) ? 'inline mr-1 -mt-1' : 'mr-2'))
+    <x-ark-icon
+        name="link"
+        :size="$small ? 'xs' : 'sm'"
+        :class="'flex-shrink-0 '.($inline ? 'inline mr-1 -mt-1' : 'mr-2')"
+    />
 </a>
