690a7bd54d47b1069c5da56ac0a952d3b6ab497f
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-03-15 10:54:40 -0600

    Use responsive images for navbar profile photos

--- resources/views/navbar/profile.blade.php
+++ resources/views/navbar/profile.blade.php
@@ -4,18 +4,18 @@
     dusk="navbar-profile-dropdown"
 >
     <x-slot name="button">
-        <span class="inline-block relative avatar-wrapper">
+        <span class="relative inline-block avatar-wrapper">
             @isset($identifier)
                 <x-ark-avatar
                     :identifier="$identifier"
-                    class="w-10 h-10 rounded-lg border-2 border-transparent md:h-11 md:w-11 hover:border-theme-primary-600"
+                    class="w-10 h-10 border-2 border-transparent rounded-lg md:h-11 md:w-11 hover:border-theme-primary-600"
                     x-bind:class="{ 'border-theme-primary-600': dropdownOpen }"
                 />
             @else
                 <div
-                    class="overflow-hidden w-12 h-12 rounded-lg md:h-16 md:w-16 md:rounded-xl"
+                    class="w-12 h-12 overflow-hidden rounded-lg md:h-16 md:w-16 md:rounded-xl"
                 >
-                    <img class="object-cover w-full h-full" src="{{ $profilePhoto }}" alt="Profile Avatar" />
+                    {{ $profilePhoto->img('', ['class' => 'object-cover w-full h-full rounded-lg md:rounded-xl', 'alt' => 'Profile Avatar']) }}
                 </div>
             @endisset
         </span>
