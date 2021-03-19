794fb68f7e050fd31bfc67076748fb693276ce17
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-03-19 20:18:47 +0000

    refactor: metatags trim image (#366)

--- resources/views/metatags.blade.php
+++ resources/views/metatags.blade.php
@@ -1,2 +1,12 @@
-<meta property="og:title" content="{{ $title }}" />
-<meta property="og:description" content="{{ $description }}">
+@props([
+    'title',
+    'description',
+    'image' => null,
+])
+
+<meta property="og:title" content="{{ trim($title) }}" />
+<meta property="og:description" content="{{ trim($description) }}">
+
+@if ($image)
+    <meta property="og:image" content="{{ trim($image) }}">
+@endif
