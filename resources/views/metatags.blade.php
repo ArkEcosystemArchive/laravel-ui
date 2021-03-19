fa7d9c9671d817e23a56b7319525692dda497789
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-03-19 21:42:21 +0100

    fix: metatags trailing slash (#367)

--- resources/views/metatags.blade.php
+++ resources/views/metatags.blade.php
@@ -5,8 +5,8 @@
 ])
 
 <meta property="og:title" content="{{ trim($title) }}" />
-<meta property="og:description" content="{{ trim($description) }}">
+<meta property="og:description" content="{{ trim($description) }}" />
 
 @if ($image)
-    <meta property="og:image" content="{{ trim($image) }}">
+    <meta property="og:image" content="{{ trim($image) }}" />
 @endif
