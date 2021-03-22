623e52ca65578e03b4e81f7c0e558a598087372e
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-03-22 10:19:44 -0600

    Prevent the notification dropdown to be larger than screens

--- resources/views/secondary-menu.blade.php
+++ resources/views/secondary-menu.blade.php
@@ -1,9 +1,9 @@
-<div x-data="{ open: false }" @keydown.window.escape="open = false" @click.away="open = false" class="inline-block relative w-full text-left">
+<div x-data="{ open: false }" @keydown.window.escape="open = false" @click.away="open = false" class="relative inline-block w-full text-left">
     <div>
         <button
             @click="open = !open"
             type="button"
-            class="inline-flex relative justify-start items-center py-3 px-4 w-full rounded-lg border border-theme-secondary-200"
+            class="relative inline-flex items-center justify-start w-full px-4 py-3 border rounded-lg border-theme-secondary-200"
         >
             @svg('menu-open', 'h-4 w-4 text-theme-secondary-900 mr-3')
             <span class="font-semibold text-theme-secondary-900">{{ $title }}</span>
@@ -17,7 +17,7 @@
         x-transition:leave="transition ease-in duration-75"
         x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95"
-        class="absolute right-0 z-10 mt-2 w-full rounded-md shadow-lg origin-top"
+        class="absolute right-0 z-10 w-full max-w-full mt-2 origin-top rounded-md shadow-lg"
         x-cloak
     >
         <div class="w-full py-4 bg-white rounded-md shadow-lg {{ $navigationClass ?? '' }}">
