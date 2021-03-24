d26dd41c3d1cbd90a71492484ead4083fad8a2ef
Maurizio <maurizio.lepora@gmail.com>
2021-03-24 17:33:13 +0100

    add border rounded to social link icons on footer component

--- resources/views/social-link.blade.php
+++ resources/views/social-link.blade.php
@@ -2,7 +2,7 @@
     href="{{ $url }}"
     target="_blank"
     rel="noopener noreferrer"
-    class="transition-default hover:text-theme-secondary-500"
+    class="transition-default hover:text-theme-secondary-500 rounded"
 >
     @svg($icon, 'w-4 h-4')
 </a>
