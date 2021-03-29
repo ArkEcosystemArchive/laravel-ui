0de05c2fa5d6bbe06168a5f39cac0bccc28be99b
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-03-29 11:38:45 -0600

    The toast should be over everything

--- resources/views/livewire/toast.blade.php
+++ resources/views/livewire/toast.blade.php
@@ -1,4 +1,4 @@
-<div class="grid fixed right-0 bottom-0 z-20 grid-rows-1 gap-2 mr-1 mb-5" style="min-width: 250px; max-width: 350px">
+<div class="fixed bottom-0 right-0 z-50 grid grid-rows-1 gap-2 mb-5 mr-1" style="min-width: 250px; max-width: 350px">
     @foreach ($toasts as $key => $toast)
         <div
             class="z-20 cursor-pointer"
