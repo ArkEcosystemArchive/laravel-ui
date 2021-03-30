b4a3b8967961036762c9bd5d95a7eb00676b832a
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-03-30 13:17:14 -0600

    Add option for modals with a fixed position

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -10,6 +10,7 @@
     'closeButtonOnly' => false,
     'wireClose' => false,
     'escToClose' => true,
+    'fixedPosition' => false,
 ])
 
 <div class="fixed inset-0 z-50 opacity-75 bg-theme-secondary-900 dark:bg-theme-secondary-800 dark:opacity-50"></div>
@@ -22,14 +23,14 @@
     @if(!$closeButtonOnly && $wireClose)
         wire:click.self="{{ $wireClose }}"
     @endif
-    class="flex overflow-y-auto fixed inset-0 z-50 md:py-10 md:px-8"
+    class="fixed inset-0 z-50 flex overflow-y-auto md:py-10 md:px-8"
     @if(!$closeButtonOnly && $escToClose)
         wire:keydown.escape="{{ $wireClose }}"
         tabindex="0"
     @endif
 >
     <div
-        class="modal-content-wrapper md:m-auto w-full {{ $class }} {{ $widthClass }}"
+        class="modal-content-wrapper @if($fixedPosition) md:mx-auto @else md:m-auto @endif w-full {{ $class }} {{ $widthClass }}"
         @if($style) style="{{ $style }}" @endif
     >
         <div class="modal-content dropdown-scrolling {{ $widthClass }}">
