7ebe2cbaddb6e1302b678252cc75433614f601f1
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-04-01 13:11:47 +0200

    wip

--- tests/Rules/TagTest.php
+++ tests/Rules/TagTest.php
@@ -22,43 +22,43 @@ it('accepts a word with numbers', function () {
     $this->assertTrue($rule->passes('tag', $tag));
 });
 
-it('doesnt accepts a word that starts with a number', function () {
+it('does not accept a word that starts with a number', function () {
     $rule = new Tag();
     $tag = '2hello';
     $this->assertFalse($rule->passes('tag', $tag));
 });
 
-it('doesnt accepts a word with an space', function () {
+it('does not accept a word with a space', function () {
     $rule = new Tag();
     $tag = 'hello world';
     $this->assertFalse($rule->passes('tag', $tag));
 });
 
-it('doesnt accepts a word with an special char', function () {
+it('does not accept a word with a special char', function () {
     $rule = new Tag();
     $tag = '#hello';
     $this->assertFalse($rule->passes('tag', $tag));
 });
 
-it('accepts a 3 chars word', function () {
+it('accepts a 3 character word', function () {
     $rule = new Tag();
     $tag = 'hel';
     $this->assertTrue($rule->passes('tag', $tag));
 });
 
-it('doesnt accepts a 2 chars word', function () {
+it('does not accept a 2 character word', function () {
     $rule = new Tag();
     $tag = 'he';
     $this->assertFalse($rule->passes('tag', $tag));
 });
 
-it('accepts a 30 chars word', function () {
+it('accepts a 30 character word', function () {
     $rule = new Tag();
     $tag = str_repeat('a', 30);
     $this->assertTrue($rule->passes('tag', $tag));
 });
 
-it('doesnt accepts a 31 chars word', function () {
+it('does not accept a 31 character word', function () {
     $rule = new Tag();
     $tag = str_repeat('a', 31);
     $this->assertFalse($rule->passes('tag', $tag));
