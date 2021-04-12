d0b02b6036bf4d0329648be9bd62bd81efd3e759
Maurizio <leMaur@users.noreply.github.com>
2021-04-12 14:17:11 +0200

    feat: add auxiliary title to text input (#419)

--- resources/views/inputs/includes/input-label.blade.php
+++ resources/views/inputs/includes/input-label.blade.php
@@ -1,10 +1,11 @@
 @props([
     'name',
     'errors',
-    'id'       => null,
-    'label'    => null,
-    'tooltip'  => null,
-    'required' => false,
+    'id'             => null,
+    'label'          => null,
+    'tooltip'        => null,
+    'required'       => false,
+    'auxiliaryTitle' => '',
 ])
 
 <label
@@ -12,6 +13,10 @@
     class="items-center input-label @error($name) input-label--error @enderror"
 >
     {{ $label ? $label : trans('forms.' . $name) }}
+
+    @if (!empty($auxiliaryTitle))
+        <span class="ml-1 text-theme-secondary-400">{{ $auxiliaryTitle }}</span>
+    @endif
 
     @if ($required)
         <div class="p-px mb-3 ml-px w-1 h-1 rounded-full bg-theme-danger-400"></div>
