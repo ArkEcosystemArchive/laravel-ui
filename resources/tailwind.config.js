f8b2176fbe0e3268c6e0a3340d5fe92899db2190
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-04-13 21:10:47 +0100

    refactor: update modal border radius

--- resources/tailwind.config.js
+++ resources/tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
     theme: {
         extend: {
             borderRadius: {
-                xl: "1rem",
+                "xl": "1rem",
+                "3xl": "1.25rem",
             },
             borderWidth: {
                 3: "3px",
