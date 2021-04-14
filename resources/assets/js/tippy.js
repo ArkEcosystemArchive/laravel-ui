67dd63fc9e377a1517c5df572925fc6481cda6d6
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-04-14 07:39:59 -0500

    feat: hide tooltips on scrolling (#422)

--- resources/assets/js/tippy.js
+++ resources/assets/js/tippy.js
@@ -1,18 +1,35 @@
 import tippy from "tippy.js";
 import "tippy.js/dist/tippy.css";
 
+const visibleTooltips = [];
+
 /** Enable tooltips for components with this data attribute, and global config options */
-tippy("[data-tippy-content]", {
+const tooltipSettings = {
     trigger: "mouseenter focus",
     duration: 0,
-});
+    onShown: (instance) => {
+        visibleTooltips.push(instance);
+    },
+    onHidden: (instance, e) => {
+        const index = visibleTooltips.findIndex((i) => i.id === instance.id);
+        if (index >= 0) {
+            visibleTooltips.splice(index, 1);
+        }
+    },
+};
+
+tippy("[data-tippy-content]", tooltipSettings);
 
 tippy("[data-tippy-hover]", {
+    ...tooltipSettings,
     touch: "hold",
     trigger: "mouseenter",
     content: (reference) => reference.dataset.tippyHover,
-    duration: 0,
 });
+
+document.addEventListener("scroll", () =>
+    visibleTooltips.forEach((instance) => instance.hide(0))
+);
 
 window.initClipboard = () => {
     tippy(".clipboard", {
