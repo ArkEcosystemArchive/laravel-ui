669d4ee72d69c994b09b12f7374d6e9a481a07d8
alfonsobries <alfonsobries@users.noreply.github.com>
2021-04-14 22:04:51 +0000

    style: resolve style guide violations

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -35,7 +35,7 @@
                 'border-b': open
             }"
         >
-            <div class="relative flex justify-between h-20">
+            <div class="flex relative justify-between h-20">
                 @include('ark::navbar.logo')
 
                 @isset($middle)
@@ -43,7 +43,7 @@
                 @endisset
 
                 <div class="flex justify-end">
-                    <div class="flex items-center justify-end flex-1 sm:items-stretch sm:justify-between">
+                    <div class="flex flex-1 justify-end items-center sm:items-stretch sm:justify-between">
                         @isset($desktop)
                             {{ $desktop }}
                         @else
@@ -51,7 +51,7 @@
                         @endisset
                     </div>
 
-                    <div class="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-4 sm:pr-0">
+                    <div class="flex inset-y-0 right-0 items-center pr-2 sm:static sm:inset-auto sm:ml-4 sm:pr-0">
                         @if(is_array($navigation))
                             @include('ark::navbar.hamburger')
                         @endif
