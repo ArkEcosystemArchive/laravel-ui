e347a46a03d93316f98ad2b094f4d4a8fd59f1ea
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-04-14 10:03:10 -0500

    Add wire:key to prevent livewire issues

--- resources/views/livewire/toast.blade.php
+++ resources/views/livewire/toast.blade.php
@@ -1,10 +1,11 @@
-<div class="grid fixed right-0 bottom-0 z-50 grid-rows-1 gap-2 mr-1 mb-5" style="min-width: 250px; max-width: 350px">
+<div class="fixed bottom-0 right-0 z-50 grid grid-rows-1 gap-2 mb-5 mr-1" style="min-width: 250px; max-width: 350px">
     @foreach ($toasts as $key => $toast)
         <div
             class="z-20 cursor-pointer"
             x-data="{ dismiss() { livewire.emit('dismissToast', '{{ $key }}' ) } }"
             x-init="() => setTimeout(() => dismiss(), 5000);"
             @click="dismiss()"
+            wire:key="{{ $key }}"
         >
             <x-ark-toast :type="$toast['type']" wire-close="dismissToast('{{ $key }}')">
                 <x-slot name='message'>
