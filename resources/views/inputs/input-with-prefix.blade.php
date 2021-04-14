1c648dd087c55c183323c87117657c9e52f49fd9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-04-14 15:04:41 +0100

    refactor: update prefix input component

--- resources/views/inputs/input-with-prefix.blade.php
+++ resources/views/inputs/input-with-prefix.blade.php
@@ -1,5 +1,23 @@
+@props([
+    'name',
+    'errors',
+    'value' => null,
+    'id' => null,
+    'label' => null,
+    'tooltip' => null,
+    'required' => false,
+    'auxiliaryTitle' => null,
+    'icon' => null,
+    'prefix' => null,
+    'inputClass' => '',
+    'noModel' => false,
+    'model' => $name,
+    'keydownEnter' => null,
+    'max' => null,
+])
+
 <div
-    x-data="{ isDirty: {{ !! ($value ?? false) ? 'true' : 'false' }} }"
+    x-data="{ isDirty: {{ $value ? 'true' : 'false' }} }"
     {{ $attributes->only('class') }}
 >
     <div class="input-group">
@@ -8,10 +26,10 @@
                 'name'           => $name,
                 'errors'         => $errors,
                 'id'             => $id ?? $name,
-                'label'          => $label ?? null,
-                'tooltip'        => $tooltip ?? null,
-                'required'       => $required ?? false,
-                'auxiliaryTitle' => $auxiliaryTitle ?? '',
+                'label'          => $label,
+                'tooltip'        => $tooltip,
+                'required'       => $required,
+                'auxiliaryTitle' => $auxiliaryTitle,
             ])
         @endunless
 
@@ -19,25 +37,36 @@
             class="input-wrapper input-wrapper-with-prefix"
             x-bind:class="{ 'input-wrapper-with-prefix--dirty': !! isDirty }"
         >
-            @include('ark::inputs.includes.input-internal-icon', [
-                'icon'     => $icon,
-                'position' => 'left',
-            ])
+            @if ($icon)
+                @include('ark::inputs.includes.input-internal-icon', [
+                    'icon'     => $icon,
+                    'position' => 'left',
+                ])
+            @elseif($prefix)
+                <div class="input-prefix">
+                    {{ $prefix }}
+                </div>
+            @endif
 
             @include('ark::inputs.includes.input-field', [
                 'name'           => $name,
                 'errors'         => $errors,
                 'id'             => $id ?? $name,
-                'inputTypeClass' => 'input-text input-text-with-prefix',
-                'inputClass'     => $inputClass ?? '',
-                'noModel'        => $noModel ?? false,
-                'model'          => $model ?? $name,
-                'keydownEnter'   => $keydownEnter ?? null,
-                'max'            => $max ?? null,
+                'inputTypeClass' => 'input-text-with-prefix',
+                'inputClass'     => $inputClass,
+                'noModel'        => $noModel,
+                'model'          => $model,
+                'keydownEnter'   => $keydownEnter,
+                'max'            => $max,
                 'attributes'     => $attributes->merge(['x-on:change' => 'isDirty = !! $event.target.value']),
             ])
 
-            @error($name) @include('ark::inputs.includes.input-error-tooltip', ['error' => $message, 'id' => $id ?? $name]) @enderror
+            @error($name)
+                @include('ark::inputs.includes.input-error-tooltip', [
+                    'error' => $message,
+                    'id' => $id ?? $name
+                ])
+            @enderror
         </div>
     </div>
 </div>
