6b29b090e108e73e675360dd516316722785d5b9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-05-04 20:48:06 +0100

    refactor: props directive

--- resources/views/divider.blade.php
+++ resources/views/divider.blade.php
@@ -1,1 +1,3 @@
-<hr class="bg-theme-secondary-300 text-theme-secondary-300 w-full border-none {{ $height ?? 'h-px' }}" />
+@props(['height' => 'h-px'])
+
+<hr class="bg-theme-secondary-300 text-theme-secondary-300 w-full border-none {{ $height }}" />
