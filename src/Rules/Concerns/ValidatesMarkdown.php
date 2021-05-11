1f0f8f377572bf1da08d8be47a2650fca4aa115a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-05-11 13:42:32 +0100

    refactor: update markdown char count validator

--- src/Rules/Concerns/ValidatesMarkdown.php
+++ src/Rules/Concerns/ValidatesMarkdown.php
@@ -9,7 +9,7 @@ trait ValidatesMarkdown
     private function getText($value): string
     {
         $html = $this->getHtml($value);
-
+        $html = $this->singleLineListItemHtml($html);
         $html = $this->removeHeadersAnchors($html);
 
         return trim(strip_tags($html));
@@ -19,6 +19,17 @@ trait ValidatesMarkdown
     {
         $regex = '/<a\s?[^>]*[^>]*>#<\/a>/siU';
         return preg_replace($regex, '', $html);
+    }
+
+    private function singleLineListItemHtml($html): string
+    {
+        return preg_replace([
+            '/(<(li|ol)( .*?)?>)+\n(<)/',
+            '/(<\/(li|ol)>)+\n(<)/',
+        ], [
+            '$1$4',
+            '$1$3',
+        ], $html);
     }
 
     private function getHtml($value): string
