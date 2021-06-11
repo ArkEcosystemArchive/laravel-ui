b36695ff94f6548bfe245ef35b2c68ef48fbae43
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-06-11 12:25:02 +0200

    fix: large info icon styling (#497)

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -10,8 +10,8 @@
     class="inline-block cursor-pointer {{ $large ? 'p-1.5' : 'p-1' }} transition-default rounded-full bg-theme-primary-100 text-theme-primary-600 dark:bg-theme-secondary-800 dark:text-theme-secondary-600 hover:text-white hover:bg-theme-primary-700 dark:hover:text-theme-secondary-800 dark:hover:bg-theme-secondary-600 {{ $class }}"
 >
     @if($type === 'question')
-        <x-ark-icon name="question-mark" size="{{ $large ? 'md' : 'xs' }}" />
+        <x-ark-icon name="question-mark" size="{{ $large ? 'sm' : 'xs' }}" />
     @else
-        <x-ark-icon name="hint" size="{{ $large ? 'md' : 'xs' }}" />
+        <x-ark-icon name="hint" size="{{ $large ? 'sm' : 'xs' }}" />
     @endif
 </div>
