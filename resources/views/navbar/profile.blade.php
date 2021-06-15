f811c326569bbffa0b0acc78464112f6f95eb35b
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-15 11:33:51 -0500

    Allow attributes and href on profile menu

--- resources/views/navbar/profile.blade.php
+++ resources/views/navbar/profile.blade.php
@@ -2,19 +2,19 @@
     wrapper-class="ml-3 whitespace-nowrap md:relative"
     :dropdown-classes="'w-full md:w-auto mt-4 '.($profileMenuClass ?? null)"
     dropdown-content-classes="bg-white rounded-xl shadow-lg dark:bg-theme-secondary-800 dark:text-theme-secondary-200 py-4"
-    button-class="overflow-hidden rounded-xl border-2 border-transparent hover:border-theme-primary-600"
+    button-class="overflow-hidden border-2 border-transparent rounded-xl hover:border-theme-primary-600"
     dusk="navbar-profile-dropdown"
 >
     <x-slot name="button">
-        <span class="inline-block relative avatar-wrapper">
+        <span class="relative inline-block avatar-wrapper">
             @isset($identifier)
                 <x-ark-avatar
                     :identifier="$identifier"
-                    class="-m-1 w-12 h-12 rounded-xl md:h-13 md:w-13"
+                    class="w-12 h-12 -m-1 rounded-xl md:h-13 md:w-13"
                     x-bind:class="{ 'border-theme-primary-600': dropdownOpen }"
                 />
             @else
-                <div class="overflow-hidden w-10 h-10 rounded-xl border-2 border-transparent md:w-11 md:h-11">
+                <div class="w-10 h-10 overflow-hidden border-2 border-transparent rounded-xl md:w-11 md:h-11">
                     {{ $profilePhoto->img('', ['class' => 'object-cover w-full h-full', 'alt' => trans('ui::general.profile_avatar_alt')]) }}
                 </div>
             @endisset
@@ -40,9 +40,16 @@
             </form>
         @else
             <a
-                href="{{ route($menuItem['route']) }}"
+                @isset($menuItem['href'])
+                    href="{{ $menuItem['href'] }}"
+                @else
+                    href="{{ route($menuItem['route']) }}"
+                @endif
                 class="dropdown-entry"
                 dusk="dropdown-entry-{{ Str::slug($menuItem['label']) }}"
+                @foreach(Arr::get($menuItem, 'attributes', []) as $attribute => $attributeValue)
+                    {{ $attribute }}="{{ $attributeValue }}"
+                @endforeach
             >
                 @if($menuItem['icon'] ?? false)
                     @svg($menuItem['icon'], 'inline w-5 mr-4')
