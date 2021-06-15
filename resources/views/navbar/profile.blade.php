0e64642c30bbdc5320be17ec630788b7084c69fd
alfonsobries <alfonsobries@users.noreply.github.com>
2021-06-15 16:35:30 +0000

    style: resolve style guide violations

--- resources/views/navbar/profile.blade.php
+++ resources/views/navbar/profile.blade.php
@@ -2,19 +2,19 @@
     wrapper-class="ml-3 whitespace-nowrap md:relative"
     :dropdown-classes="'w-full md:w-auto mt-4 '.($profileMenuClass ?? null)"
     dropdown-content-classes="bg-white rounded-xl shadow-lg dark:bg-theme-secondary-800 dark:text-theme-secondary-200 py-4"
-    button-class="overflow-hidden border-2 border-transparent rounded-xl hover:border-theme-primary-600"
+    button-class="overflow-hidden rounded-xl border-2 border-transparent hover:border-theme-primary-600"
     dusk="navbar-profile-dropdown"
 >
     <x-slot name="button">
-        <span class="relative inline-block avatar-wrapper">
+        <span class="inline-block relative avatar-wrapper">
             @isset($identifier)
                 <x-ark-avatar
                     :identifier="$identifier"
-                    class="w-12 h-12 -m-1 rounded-xl md:h-13 md:w-13"
+                    class="-m-1 w-12 h-12 rounded-xl md:h-13 md:w-13"
                     x-bind:class="{ 'border-theme-primary-600': dropdownOpen }"
                 />
             @else
-                <div class="w-10 h-10 overflow-hidden border-2 border-transparent rounded-xl md:w-11 md:h-11">
+                <div class="overflow-hidden w-10 h-10 rounded-xl border-2 border-transparent md:w-11 md:h-11">
                     {{ $profilePhoto->img('', ['class' => 'object-cover w-full h-full', 'alt' => trans('ui::general.profile_avatar_alt')]) }}
                 </div>
             @endisset
