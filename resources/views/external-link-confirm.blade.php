9a11bc958742ec0f298b1932159ff20a24b75b41
Highjhacker <jolanbeer@gmail.com>
2021-06-23 15:53:55 +0200

    refactor: requested changes

--- resources/views/external-link-confirm.blade.php
+++ resources/views/external-link-confirm.blade.php
@@ -6,76 +6,81 @@
 
 @php($identifier = md5($url . '-' . Str::random(8)))
 
-<a
-    href="javascript:;"
-    x-data="{
+<div x-data="{
         openModal() {
             Livewire.emit('openModal', '{{ $identifier }}')
         },
         redirect() {
             window.open('{{ $url }}', '_blank')
         },
+        hasConfirmedLinkWarning() {
+            return localStorage.getItem('confirm_link_warning') == 'true';
+        },
         hasDisabledLinkWarning() {
             return localStorage.getItem('disable_link_warning') == 'true';
         }
     }"
-    class="inline-flex items-center space-x-2 font-semibold whitespace-nowrap cursor-pointer link"
-    @click="hasDisabledLinkWarning() ? redirect() : openModal()"
 >
-    <span>{{ $text ?? $slot ?? '' }}</span>
-
-    @unless($noIcon)
-        <x-ark-icon name="link" size="sm" class="mx-2" />
-    @endunless
-</a>
-
-@push('footer')
-    <x-ark-js-modal
-        :name="$identifier"
-        class="w-full max-w-2xl text-left rounded-xl"
-        title-class="header-2"
-        buttons-style="flex justify-end space-x-3"
-        init
+    <a
+        href="javascript:;"
+        class="inline-flex items-center space-x-2 font-semibold whitespace-nowrap cursor-pointer link"
+        @click="hasDisabledLinkWarning() ? redirect() : openModal()"
     >
-        @slot('title')
-            @lang('generic.external_link')
-        @endslot
+        <span>{{ $text ?? $slot ?? '' }}</span>
 
-        @slot('description')
-            <div class="flex flex-col mt-8 space-y-4 whitespace-normal">
-                <div class="font-semibold text-theme-secondary-900">
-                    <x-ark-alert type="warning" :message="$url" />
+        @unless($noIcon)
+            <x-ark-icon name="link" size="sm" class="mx-2" />
+        @endunless
+    </a>
+
+    @push('footer')
+        <x-ark-js-modal
+            :name="$identifier"
+            class="w-full max-w-2xl text-left rounded-xl"
+            title-class="header-2"
+            buttons-style="flex justify-end space-x-3"
+            init
+        >
+            @slot('title')
+                @lang('generic.external_link')
+            @endslot
+
+            @slot('description')
+                <div class="flex flex-col mt-8 space-y-4 whitespace-normal">
+                    <div class="font-semibold text-theme-secondary-900">
+                        <x-ark-alert type="warning" :message="$url" />
+                    </div>
+                    <p>@lang('generic.external_link_disclaimer')</p>
+
+                    <x-ark-checkbox
+                        name="confirmation"
+                        alpine="localStorage.getItem('confirm_link_warning') == 'true' ? localStorage.setItem('confirm_link_warning', false) : localStorage.setItem('confirm_link_warning', true)"
+                    >
+                        @slot('label')
+                            @lang('ui::forms.do_not_show_message_again')
+                        @endslot
+                    </x-ark-checkbox>
                 </div>
-                <p>@lang('generic.external_link_disclaimer')</p>
+            @endslot
 
-                <x-ark-checkbox
-                    name="confirmation"
-                    alpine="localStorage.getItem('disable_link_warning') == 'true' ? localStorage.setItem('disable_link_warning', false) : localStorage.setItem('disable_link_warning', true)"
+            @slot('buttons')
+                <button
+                    class="button-secondary"
+                    @click="hide"
                 >
-                    @slot('label')
-                        @lang('ui::forms.do_not_show_message_again')
-                    @endslot
-                </x-ark-checkbox>
-            </div>
-        @endslot
+                    @lang('actions.back')
+                </button>
 
-        @slot('buttons')
-            <button
-                class="button-secondary"
-                @click="hide"
-            >
-                @lang('actions.back')
-            </button>
-
-            <a
-                target="_blank"
-                rel="noopener nofollow"
-                class="cursor-pointer button-primary"
-                href="{{ $url }}"
-                @click="hide"
-            >
-                @lang('actions.follow_link')
-            </a>
-        @endslot
-    </x-ark-js-modal>
-@endpush
+                <a
+                    target="_blank"
+                    rel="noopener nofollow"
+                    class="cursor-pointer button-primary"
+                    href="{{ $url }}"
+                    @click="hide(); localStorage.getItem('confirm_link_warning') == 'true' ? localStorage.setItem('disable_link_warning', false) : localStorage.setItem('disable_link_warning', true)"
+                >
+                    @lang('actions.follow_link')
+                </a>
+            @endslot
+        </x-ark-js-modal>
+    @endpush
+</div>
