16cbe1b8e1b1bec694bb399ae1e92f0065d6bad6
alfonsobries <alfonsobries@users.noreply.github.com>
2021-06-29 15:20:41 +0000

    style: resolve style guide violations

--- resources/assets/js/markdown-editor/markdown-editor.js
+++ resources/assets/js/markdown-editor/markdown-editor.js
@@ -1,6 +1,6 @@
-import Editor from '@toast-ui/editor';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import 'codemirror/lib/codemirror.css';
+import Editor from "@toast-ui/editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import "codemirror/lib/codemirror.css";
 
 import {
     simplecastPlugin,
