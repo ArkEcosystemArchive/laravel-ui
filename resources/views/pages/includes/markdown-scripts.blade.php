fdc196cd40a47126872701c5fda1cf3c1ee86eb5
alfonsobries <alfonsobries@gmail.com>
2021-06-29 10:15:32 -0500

    Use local version of the toastui package

--- resources/views/pages/includes/markdown-scripts.blade.php
+++ resources/views/pages/includes/markdown-scripts.blade.php
@@ -1,4 +1,1 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.48.4/codemirror.min.css" />
-<script src="https://uicdn.toast.com/editor/2.5.2/toastui-editor-all.min.js" defer></script>
-<link rel="stylesheet" href="https://uicdn.toast.com/editor/2.5.2/toastui-editor-only.min.css" />
-<script src="{{ mix('js/markdown-editor.js')}}"></script>
+<script src="{{ mix('js/markdown-editor.js') }}"></script>
