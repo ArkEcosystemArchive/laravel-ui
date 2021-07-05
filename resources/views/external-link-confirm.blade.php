7d8d56abd7125334afc9cb2f087865fc9f7e2832
Highjhacker <jolanbeer@gmail.com>
2021-07-05 08:54:58 +0200

    refactor: requested changes

--- resources/views/external-link-confirm.blade.php
+++ resources/views/external-link-confirm.blade.php
@@ -27,11 +27,11 @@
         @click="hasDisabledLinkWarning() ? redirect() : openModal()"
     >
         <span>{{ $text ?? $slot ?? '' }}</span>
+
+        @unless($noIcon)
+            <x-ark-icon name="link" size="sm" class="inline flex-shrink-0 mr-2 ml-1 -mt-1" />
+        @endunless
     </a>
-    @unless($noIcon)
-        <x-ark-icon name="link" size="sm" class="inline flex-shrink-0 mr-2 ml-1 -mt-1" />
-    @endunless
-</a>
 
     @push('footer')
         <x-ark-js-modal
