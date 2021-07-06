bb37b1dd1916e44d6af82af5261c2d99dfd9ba05
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-07-06 10:21:51 +0100

    move strip spaces method to a concern

--- src/Rules/MarkdownWithContent.php
+++ src/Rules/MarkdownWithContent.php
@@ -2,12 +2,14 @@
 
 namespace ARKEcosystem\UserInterface\Rules;
 
+use ARKEcosystem\UserInterface\Rules\Concerns\ValidatesEmptyString;
 use ARKEcosystem\UserInterface\Rules\Concerns\ValidatesMarkdown;
 use Illuminate\Contracts\Validation\Rule;
 
 class MarkdownWithContent implements Rule
 {
     use ValidatesMarkdown;
+    use ValidatesEmptyString;
 
     /**
      * Determine if the validation rule passes.
@@ -21,14 +23,6 @@ class MarkdownWithContent implements Rule
         $text = $this->stripZeroWidthSpaces($this->getText($value));
 
         return strlen($text) > 0;
-    }
-
-    public function stripZeroWidthSpaces(string $text): string
-    {
-        // Zero-width characters to remove, source: http://jkorpela.fi/chars/spaces.html
-        $regex = '/[\x{2000}-\x{200D}\x{FEFF}\x{0020}\x{00A0}\x{3000}\x{205F}\x{202F}\x{00A0}\x{180B}-\x{180F}]/u';
-
-        return preg_replace($regex, '', $text);
     }
 
     /**
