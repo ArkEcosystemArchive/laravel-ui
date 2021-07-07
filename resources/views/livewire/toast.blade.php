7350da4fbd5aae3e583c079f492b19c98565874d
alfonsobries <alfonsobries@gmail.com>
2021-07-07 11:44:47 -0500

    Toast with icons design

--- resources/views/livewire/toast.blade.php
+++ resources/views/livewire/toast.blade.php
@@ -1,4 +1,4 @@
-<div class="grid fixed right-0 bottom-0 z-50 grid-rows-1 gap-2 p-5">
+<div class="fixed bottom-0 right-0 z-50 grid max-w-4xl grid-rows-1 gap-3 p-5">
     @foreach ($toasts as $key => $toast)
         <div
             class="z-20 cursor-pointer"
@@ -9,7 +9,7 @@
         >
             <x-ark-toast :type="$toast['type']" wire-close="dismissToast('{{ $key }}')">
                 <x-slot name='message'>
-                    <div class="px-3">{!! $toast['message'] !!}</div>
+                    {!! $toast['message'] !!}
                 </x-slot>
             </x-ark-toast>
         </div>
--- resources/views/toast.blade.php
+++ resources/views/toast.blade.php
@@ -1,12 +1,63 @@
-<div class="toast-{{ $type }} flex items-center {{ $class ?? '' }}">
-    <div class="flex-1">{{ $message }}</div>
+@props([
+    'type' => 'info',
+    'message' => '',
+    'wireClose' => false,
+    'alpineClose' => false,
+])
 
-    @if (($wireClose ?? false) || ($alpineClose ?? false))
-        <div
-            @if ($wireClose ?? false) wire:click="{{ $wireClose }}" @endif
-            @if ($alpineClose ?? false) @click="{{ $alpineClose }}" @endif
+@php
+    $colorClass = Arr::get([
+        'warning' => 'bg-theme-warning-50',
+        'error' => 'bg-theme-danger-50',
+        'danger' => 'bg-theme-danger-50',
+        'success' => 'bg-theme-success-50',
+        'info' => 'bg-theme-primary-50',
+        'hint' => 'bg-theme-hint-50',
+    ], $type, 'bg-theme-primary-50');
+
+    $iconClass = Arr::get([
+        'warning' => 'bg-theme-warning-600',
+        'error' => 'bg-theme-danger-400',
+        'danger' => 'bg-theme-danger-400',
+        'success' => 'bg-theme-success-600',
+        'info' => 'bg-theme-primary-600',
+        'hint' => 'bg-theme-hint-500',
+    ], $type, 'bg-theme-primary-600');
+
+    $icon = Arr::get([
+        'warning' => 'toasts.warning',
+        'error' => 'toasts.danger',
+        'danger' => 'toasts.danger',
+        'success' => 'toasts.success',
+        'info' => 'toasts.info',
+        'hint' => 'toasts.hint',
+    ], $type, 'info');
+
+    $closeButtonClass = Arr::get([
+        'warning' => 'bg-theme-warning-100 hover:bg-theme-warning-200',
+        'error' => 'bg-theme-danger-100 hover:bg-theme-danger-200',
+        'danger' => 'bg-theme-danger-100 hover:bg-theme-danger-200',
+        'success' => 'bg-theme-success-100 hover:bg-theme-success-200',
+        'info' => 'bg-theme-primary-100 hover:bg-theme-primary-200',
+        'hint' => 'bg-theme-hint-100 hover:bg-theme-hint-200',
+    ], $type, 'bg-theme-primary-100 hover:bg-theme-primary-200');
+@endphp
+
+<div {{ $attributes->merge(['class' => 'flex flex-col sm:flex-row sm:space-x-4 items-center p-4 text-sm select-none text-theme-secondary-900 rounded-xl relative ' . $colorClass]) }}>
+    <span class="flex items-center justify-center rounded text-white w-11 h-11 flex-shrink-0 {{ $iconClass }}">
+        <x-ark-icon :name="$icon" />
+    </span>
+
+    <div class="mt-4 text-center sm:pr-6 sm:mt-0 sm:text-left">{{ $message }}</div>
+
+    @if ($wireClose || $alpineClose)
+        <button
+            @if ($wireClose) wire:click="{{ $wireClose }}" @endif
+            @if ($alpineClose) @click="{{ $alpineClose }}" @endif
+            type="button"
+            class="absolute top-0 right-0 m-4 sm:m-0 sm:top-auto sm:right-auto sm:relative flex items-center justify-center w-11 h-11 rounded flex-shrink-0 {{ $closeButtonClass }}"
         >
-            @svg('close', 'toast-close')
-        </div>
+            <x-ark-icon name="close" />
+        </button>
     @endif
 </div>
