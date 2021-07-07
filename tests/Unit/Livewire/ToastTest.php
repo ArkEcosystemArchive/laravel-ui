eeebca6bb74213fb0f6487ea076b2b191e08d432
alfonsobries <alfonsobries@gmail.com>
2021-07-07 12:17:35 -0500

    Toast tests

--- tests/Unit/Livewire/ToastTest.php
+++ tests/Unit/Livewire/ToastTest.php
@@ -7,7 +7,24 @@ it('can_see_a_toast', function () {
     Livewire::test(Toast::class)
         ->emit('toastMessage', ['Hello', 'info'])
         ->assertSee('Hello')
-        ->assertSee('info');
+        ->assertSee('button')
+        ->assertSee('span');
+});
+
+it('hides the button if style is `onlyicon`', function () {
+    Livewire::test(Toast::class)
+        ->emit('toastMessage', ['Hello', 'info', 'onlyicon'])
+        ->assertSee('Hello')
+        ->assertDontSee('button')
+        ->assertSee('span');
+});
+
+it('hides the button and the icon if style is `simple`', function () {
+    Livewire::test(Toast::class)
+        ->emit('toastMessage', ['Hello', 'info', 'simple'])
+        ->assertSee('Hello')
+        ->assertDontSee('button')
+        ->assertDontSee('span');
 });
 
 it('can_dismiss_a_toast', function () {
@@ -15,9 +32,12 @@ it('can_dismiss_a_toast', function () {
         ->set('toasts', ['test123' => [
             'message' => 'Hello',
             'type'    => 'info',
+            'style'   => 'regular',
         ]])
         ->assertSee('Hello')
-        ->assertSee('info')
+        ->assertSee('button')
+        ->assertSee('span')
         ->call('dismissToast', 'test123')
-        ->assertDontSee('Hello');
+        ->assertDontSee('button')
+        ->assertDontSee('span');
 });
