6f266a4909e8e2cddaac8a08ab1b91031bbf5234
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-07-14 11:34:21 +0100

    refactor: status circle shallow tooltip

--- resources/views/status-circle-shallow.blade.php
+++ resources/views/status-circle-shallow.blade.php
@@ -1,3 +1,10 @@
+@props([
+    'type',
+    'size'          => 'base',
+    'containerSize' => 'base',
+    'tooltip'       => null,
+])
+
 @php
     $size = [
         'sm'   => in_array($type, ['success', 'failed', 'error']) ? 'h-1 w-1' : 'w-2 h-2',
@@ -5,7 +12,7 @@
         'lg'   => in_array($type, ['success', 'failed', 'error']) ? 'h-4 w-4' : 'w-5 h-5',
         'xl'   => in_array($type, ['success', 'failed', 'error']) ? 'h-5 w-5' : 'w-6 h-6',
         'base' => in_array($type, ['success', 'failed', 'error']) ? 'h-2 w-2' : 'h-3 w-3',
-    ][$size ?? 'base'];
+    ][$size];
 
     $containerSize = [
         'sm'   => 'w-4 h-4',
@@ -14,7 +21,7 @@
         'xl'   => 'w-12 h-12',
         '2xl'  => 'w-14 h-14',
         'base' => 'w-6 h-6',
-    ][$containerSize ?? 'base'];
+    ][$containerSize];
 
     $icon = Arr::get([
         'success' => 'checkmark',
@@ -89,6 +96,11 @@
     ], $type, 'text-theme-secondary-700');
 @endphp
 
-<div class="flex items-center justify-center flex-shrink-0 border-2 rounded-full {{ $borderColor }} {{ $containerSize }}">
+<div
+    class="flex items-center justify-center flex-shrink-0 border-2 rounded-full {{ $borderColor }} {{ $containerSize }}"
+    @if ($tooltip)
+        data-tippy-content="{{ $tooltip }}"
+    @endif
+>
     @svg($icon, $iconColor.' '.$size)
 </div>
