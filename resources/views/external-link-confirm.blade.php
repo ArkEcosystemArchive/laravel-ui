3404b952d7e3885b4276e8b14e35b8ff9ea7f43a
alfonsobries <alfonsobries@gmail.com>
2021-07-20 10:34:12 -0500

    Add top margin

--- resources/views/external-link-confirm.blade.php
+++ resources/views/external-link-confirm.blade.php
@@ -19,7 +19,7 @@
     <span>{{ $text ?? $slot ?? '' }}</span>
 
     @unless($noIcon)
-        <x-ark-icon name="link" size="sm" class="inline flex-shrink-0 ml-1" />
+        <x-ark-icon name="link" size="sm" class="inline flex-shrink-0 ml-1 -mt-0.5" />
     @endunless
 </a>
 
