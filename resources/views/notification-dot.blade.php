9aef0f23147a13e77d09781e304e20418ca69636
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-08-02 12:40:57 +0100

    notification dot use single class prop

--- resources/views/notification-dot.blade.php
+++ resources/views/notification-dot.blade.php
@@ -1,10 +1,9 @@
 @props([
-    'size'       => 1,
-    'background' => 'p-1 bg-white group-hover:bg-theme-primary-100',
-    'position'   => 'mr-2 -mt-4',
-    'color'      => 'bg-theme-danger-400 border-theme-danger-400',
+    'size'  => 1,
+    'class' => 'mr-2 -mt-4 p-1 bg-white group-hover:bg-theme-primary-100',
+    'color' => 'bg-theme-danger-400 border-theme-danger-400',
 ])
 
-<span class="absolute right-0 block rounded-full transition-default {{ $background }} {{ $position }}">
+<span class="absolute right-0 block rounded-full transition-default {{ $class }}">
     <span class="block w-{{ $size }} h-{{ $size }} rounded-full border-3 {{ $color }}"></span>
 </span>
