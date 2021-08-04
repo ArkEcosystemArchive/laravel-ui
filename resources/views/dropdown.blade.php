148c92439e0c598576a5ba6fe02ac98f5c8a7198
alfonsobries <alfonsobries@gmail.com>
2021-08-04 16:46:34 -0500

    Allow to change the origin on the dropdown

--- resources/views/dropdown.blade.php
+++ resources/views/dropdown.blade.php
@@ -4,6 +4,7 @@
     'buttonClassExpanded'    => 'text-theme-primary-500',
     'buttonClass'            => 'text-theme-secondary-400 hover:text-theme-primary-500',
     'dropdownClasses'        => 'w-40',
+    'dropdownOriginClass'    => 'origin-top-right',
     'wrapperClass'           => '',
     'fullScreen'             => false,
     'dusk'                   => false,
@@ -37,8 +38,8 @@
     <div>
         <button
             @click="{{ $dropdownProperty }} = !{{ $dropdownProperty }}"
-            :class="{ '{{ $buttonClassExpanded }}' : {{ $dropdownProperty }} }"
             class="flex items-center focus:outline-none dropdown-button transition-default {{ $buttonClass }}"
+            :class="{ '{{ $buttonClassExpanded }}' : {{ $dropdownProperty }} }"
             @if($buttonTooltip)
                 data-tippy-content="{{ $buttonTooltip }}"
             @endif
@@ -59,7 +60,7 @@
         x-transition:leave="transition ease-in duration-75"
         x-transition:leave-start="transform opacity-100 scale-100"
         x-transition:leave-end="transform opacity-0 scale-95"
-        class="origin-top-right absolute right-0 mt-2 z-10 dropdown {{ $dropdownClasses }} {{ $fullScreen ? 'w-screen -mx-8 md:w-auto md:mx-0' : '' }}"
+        class=" absolute right-0 mt-2 z-10 dropdown {{ $dropdownClasses }} {{ $fullScreen ? 'w-screen -mx-8 md:w-auto md:mx-0' : '' }}"
         @if ($height) data-height="{{ $height }}" @endif
     >
         <div class="{{ $dropdownContentClasses }}" x-cloak>
