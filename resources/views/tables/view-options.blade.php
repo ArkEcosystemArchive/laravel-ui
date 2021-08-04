b7e8693120c4c3564e59f6ec8d8e3367f6a21c38
Maurizio <maurizio.lepora@gmail.com>
2021-08-04 09:53:51 +0200

    refactor: manage `disabled` state for table view-options

--- resources/views/tables/view-options.blade.php
+++ resources/views/tables/view-options.blade.php
@@ -2,28 +2,33 @@
     'showMobile'        => false,
     'selectedClasses'   => 'text-theme-danger-400 border-theme-danger-100',
     'unselectedClasses' => 'text-theme-info-300 border-white',
+    'disabled'          => false,
 ])
 
 <div class="items-center @if($showMobile) flex @else hidden md:flex @endif">
-    <div
+    <button
+        type="button"
         :class="{
             '{{ $selectedClasses }}': tableView === 'grid',
             '{{ $unselectedClasses }}': tableView !== 'grid',
         }"
-        class="py-2 px-3 cursor-pointer border-b-3"
-        @click="tableView = 'grid'"
+        class="py-2 px-3 border-b-3"
+        @unless ($disabled) @click="tableView = 'grid'" @endunless
+        @if ($disabled) disabled @endif
     >
         <x-ark-icon name="grid" />
-    </div>
+    </button>
 
-    <div
+    <button
+        type="button"
         :class="{
             '{{ $selectedClasses }}': tableView === 'list',
             '{{ $unselectedClasses }}': tableView !== 'list',
         }"
-        class="py-2 px-3 cursor-pointer border-b-3"
-        @click="tableView = 'list'"
+        class="py-2 px-3 border-b-3"
+        @unless ($disabled) @click="tableView = 'list'" @endunless
+        @if ($disabled) disabled @endif
     >
         <x-ark-icon name="list" />
-    </div>
+    </button>
 </div>
