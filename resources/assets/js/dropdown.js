a18da54330d0142cacccc5723bf306e0b4b646e9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-08-09 15:20:19 +0100

    remove js debug

--- resources/assets/js/dropdown.js
+++ resources/assets/js/dropdown.js
@@ -27,26 +27,18 @@ const Dropdown = {
     repositionOutOfBounds(container) {
         console.log('repositionOutOfBounds');
         const dropdown = container.querySelector('.dropdown');
-        // dropdown.style.marginTop = null;
-        // container.style.position = null;
         dropdown.style.bottom = null;
         dropdown.style.top = null;
 
         this.$nextTick(() => {
-            const bounds   = dropdown.getBoundingClientRect();
+            const bounds = dropdown.getBoundingClientRect();
 
             const dropdownBottom =
                 bounds.top +
                 document.documentElement.scrollTop +
-                parseInt(dropdown.getBoundingClientRect().height); // -
-                // (parseInt(dropdown.style.marginTop) || 0);
-
-            console.log(dropdownBottom, parseInt(dropdown.getBoundingClientRect().height));
+                parseInt(bounds.height);
 
             if (dropdownBottom > document.body.clientHeight) {
-                // dropdown.style.marginTop = `-${dropdownBottom - document.body.clientHeight + 40}px`;
-                // console.log(dropdown.style.marginTop);
-
                 container.style.position = 'static';
                 dropdown.style.bottom = '0';
                 dropdown.style.top = 'auto';
@@ -69,11 +61,6 @@ const Dropdown = {
                         this.$nextTick(() => Dropdown.onClosed.call(this, settings));
                     }
                 });
-            },
-
-            repositionOnClick() {
-                // this.$nextTick(() => console.log('wut', this.$el));
-                // Dropdown.repositionOutOfBounds.call(this, this.$el);
             },
 
             toggle() {
