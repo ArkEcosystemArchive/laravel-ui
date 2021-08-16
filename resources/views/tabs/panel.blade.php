b5e73be83a565fda7cbd3d253e420dbdc001e7e8
Maurizio <maurizio.lepora@gmail.com>
2021-08-16 15:43:36 +0200

    wip

--- resources/views/tabs/panel.blade.php
+++ resources/views/tabs/panel.blade.php
@@ -1,5 +1,5 @@
 @props(['name'])
 
-<div role="tabpanel" id="tab-panel-{{ $name }}" x-show="selected === '{{ $name }}'" {{ $attributes }}>
+<div role="tabpanel" id="tab-panel-{{ $name }}" x-show="selected === '{{ $name }}'" {{ $attributes->merge(['class' => 'tab-panel']) }}>
     {{ $slot }}
 </div>
