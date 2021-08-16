02478156f1b27c624567495e52c2e32134d3aedf
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-08-16 03:05:25 -0500

    feat: input with suffix (#606)

--- src/UserInterfaceServiceProvider.php
+++ src/UserInterfaceServiceProvider.php
@@ -176,6 +176,7 @@ class UserInterfaceServiceProvider extends ServiceProvider
         Blade::component('ark::inputs.input', 'ark-input');
         Blade::component('ark::inputs.input-with-icon', 'ark-input-with-icon');
         Blade::component('ark::inputs.input-with-prefix', 'ark-input-with-prefix');
+        Blade::component('ark::inputs.input-with-suffix', 'ark-input-with-suffix');
         Blade::component('ark::inputs.radio', 'ark-radio');
         Blade::component('ark::inputs.textarea', 'ark-textarea');
         Blade::component('ark::inputs.toggle', 'ark-toggle');
