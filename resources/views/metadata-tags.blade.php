ab3ef98f5f83dfffce8a941426f2dd5e6f63dab8
alfonsobries <alfonsobries@gmail.com>
2021-08-23 10:51:06 -0500

    spacing

--- resources/views/metadata-tags.blade.php
+++ resources/views/metadata-tags.blade.php
@@ -6,15 +6,11 @@
 
 <title>{{ $title }}</title>
 
-
 <meta name="description" content="{{ $description }}" />
-
 
 <meta property="og:title" content="{{ $metaTitle ?? $title }}" />
 
-
 <meta property="og:description" content="{{ $description }}" />
-
 
 @if ($image)
     <meta property="og:image" content="{{ $image }}" />
