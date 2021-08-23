18640ce68446ba85bf8e98cb9264ed3858167421
alfonsobries <alfonsobries@gmail.com>
2021-08-23 10:50:35 -0500

    Remove metadata-title component

--- resources/views/metadata-title.blade.php
+++
@@ -1,3 +0,0 @@
-{{-- Uses a regular `echo` expression since blade syntax creates unexpected HTML
-entities like `&#039;` that doesnt work correctly when shared for example. --}}
-<title><?php echo htmlentities(trim($slot)) ?></title>
