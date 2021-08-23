0c4ddb076e793f7b09c509a229b215262294d7e8
leMaur <leMaur@users.noreply.github.com>
2021-08-23 10:25:04 +0000

    style: resolve style guide violations

--- resources/assets/js/tabs.js
+++ resources/assets/js/tabs.js
@@ -8,14 +8,23 @@ const Tabs = (defaultSelected = "", extraData = {}, onSelected = () => {}) => {
             this.onSelected(name);
         },
         keyboard(e) {
-            const tabs = e.target.closest("[role=tablist]").querySelectorAll("[role=tab]");
+            const tabs = e.target
+                .closest("[role=tablist]")
+                .querySelectorAll("[role=tab]");
             let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
             // On some screen readers the down arrow moves user to the next element.
             // We intercept the down arrow key and move focus on the open panel.
-            let dir = e.keyCode === 37 ? index - 1 : e.keyCode === 39 ? index + 1 : e.keyCode === 40 ? 'down' : null;
+            let dir =
+                e.keyCode === 37
+                    ? index - 1
+                    : e.keyCode === 39
+                    ? index + 1
+                    : e.keyCode === 40
+                    ? "down"
+                    : null;
 
             if (dir !== null) {
-                if (dir === 'down') {
+                if (dir === "down") {
                     this.select(e.currentTarget.id.replace("tab-", ""));
                 } else if (tabs[dir]) {
                     this.select(tabs[dir].id.replace("tab-", ""));
