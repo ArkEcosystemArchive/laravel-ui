385e0ac7af82c6c314d1b7cc4abdce1fa854c656
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-08-24 14:32:04 +0000

    fix: normalize external link component (#612)

--- resources/views/external-link.blade.php
+++ resources/views/external-link.blade.php
@@ -1,26 +1,24 @@
 @props([
     'url',
     'text',
-    'class'     => 'link font-semibold',
-    'inline'    => false,
-    'allowWrap' => false,
+    'class'     => 'link font-semibold inline break-words',
     'small'     => false,
     'noIcon'    => false,
 ])
 
 <a
     href="{{ $url }}"
-    class="{{ $class }} {{ $inline ? 'inline space-x-1' : 'flex items-center space-x-2' }} {{ $allowWrap ? '' : 'whitespace-nowrap' }}"
+    class="{{ $class }}"
     target="_blank"
     rel="noopener nofollow noreferrer"
 >
-    <span>{{ $slot ?? $text }}</span>
+    <span>{{ isset($slot) && trim($slot) ? $slot : $text }}</span>
 
     @unless($noIcon)
         <x-ark-icon
             name="link"
             :size="$small ? 'xs' : 'sm'"
-            :class="'flex-shrink-0 '.($inline ? 'inline mr-1 -mt-1' : 'mr-2')"
+            :class="'flex-shrink-0 inline relative ml-0.5 ' . ($small ? '-top-1 -mt-0.5' : '-mt-1.5')"
         />
     @endunless
 </a>
