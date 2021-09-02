e1d4a418c360238d38658cf1e22805e85dd58843
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-09-02 20:21:45 +0100

    refactor: footer copyright text property

--- resources/views/footer-bar-desktop.blade.php
+++ resources/views/footer-bar-desktop.blade.php
@@ -1,10 +1,16 @@
 @props([
     'isArkProduct' => true,
-    'noBorder' => '',
-    'copyClass' => '',
+    'noBorder'     => '',
+    'copyClass'    => '',
+    'copyText'     => null,
 ])
 
 <div class="flex items-center justify-between @unless ($noBorder) border-t border-theme-secondary-800 @endunless">
-    <x-ark-footer-copyright :is-ark-product="$isArkProduct" class="{{ $copyClass }}" />
+    <x-ark-footer-copyright
+        :is-ark-product="$isArkProduct"
+        :copy-text="$copyText"
+        class="{{ $copyClass }}"
+    />
+
     <x-ark-footer-social />
 </div>
--- resources/views/footer-bar-mobile.blade.php
+++ resources/views/footer-bar-mobile.blade.php
@@ -1,10 +1,15 @@
 @props([
     'isArkProduct' => true,
-    'class' => '',
-    'copyClass' => '',
+    'class'        => '',
+    'copyClass'    => '',
+    'copyText'     => null,
 ])
 
 <div class="flex flex-col {{ $class }}">
-    <x-ark-footer-copyright :is-ark-product="$isArkProduct" class="{{ $copyClass }}" />
+    <x-ark-footer-copyright
+        :is-ark-product="$isArkProduct"
+        :copy-text="$copyText"
+        class="{{ $copyClass }}"
+    />
     <x-ark-footer-social />
 </div>
