882c1f208e034e80a2311cc15d64c066d90d2a2b
Highjhacker <jolanbeer@gmail.com>
2021-09-10 17:32:03 +0200

    refactor: extra attributes for details-box-mobile component

--- resources/views/details-box-mobile.blade.php
+++ resources/views/details-box-mobile.blade.php
@@ -1,8 +1,8 @@
 <div class="flex justify-between">
     <div class="{{ $titleClass ?? 'w-32' }}">{{ $title }}</div>
 
-    <div class="flex items-center space-x-3">
-        <div>{{ $slot }}</div>
+    <div class="flex items-center space-x-3 {{ $wrapperClass ?? false }}">
+        <div class="{{ $slotClass ?? 'false' }}">{{ $slot }}</div>
 
         @if ($icon ?? false)
             <x-ark-icon :name="$icon" class="{{ $iconClass ?? '' }}" />
