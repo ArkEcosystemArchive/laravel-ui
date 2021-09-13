770056f7514544d951637603f9fb1bd55752daa9
alfonsobries <alfonsobries@gmail.com>
2021-09-13 10:37:34 -0500

    Move all right reserved to a term

--- resources/views/simple-footer.blade.php
+++ resources/views/simple-footer.blade.php
@@ -1,3 +1,3 @@
 <div class="flex items-center justify-center w-full h-16 text-sm text-theme-secondary-500 bg-theme-secondary-100">
-    <span>2017 - {{ date('Y') }} &copy; <a href="https://ark.io/">ARK.io</a> | All Rights Reserved</span>
+    <span>2017 - {{ date('Y') }} &copy; <a href="https://ark.io/">ARK.io</a> | @lang('ui::general.all_rights_reserved')</span>
 </div>
