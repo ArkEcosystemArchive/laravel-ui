b0a9db24f3417f778aba3d47f3dffbdca7f8793a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-09-23 13:38:54 +0100

    refactor: js date formats

--- src/Support/DateFormat.php
+++ src/Support/DateFormat.php
@@ -11,4 +11,12 @@ class DateFormat
     const TIME_SHORT = 'd M Y H:i';
 
     const TIME_PARENTHESES = 'd M Y (H:i:s)';
+
+    const DATE_JS = 'D MMM YYYY';
+
+    const TIME_JS = 'D MMM YYYY HH:mm:ss';
+
+    const TIME_SHORT_JS = 'D MMM YYYY HH:mm';
+
+    const TIME_PARENTHESES_JS = 'D MMM YYYY (HH:mm:ss)';
 }
